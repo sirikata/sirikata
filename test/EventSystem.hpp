91b09bb4323d86c96772a4506fb6d302fbc465d1
Daniel Horn <hellcatv@users.sourceforge.net>
2009-01-01 11:49:55 +0000

    test all event types, not just EventA

--- test/EventSystem.hpp
+++ test/EventSystem.hpp
@@ -57,6 +57,7 @@ public:
         delete mManager;
     }
     Task::EventResponse doNotCall(Task::GenEventManager::EventPtr){
+        TS_FAIL("Called incorrect function");
         mFail=true;
         return Task::EventResponse::del();
     }
@@ -68,48 +69,94 @@ public:
         mCount++;
         return Task::EventResponse::nop();
     }    
-    void testDeliveryA( void )
+    void deliveryABCDE( int whichevent )
     {
-        Task::GenEventManager::EventPtr a(new EventA(1));
-        Task::GenEventManager::EventPtr b(new EventB(2));
-        Task::GenEventManager::EventPtr c(new EventC(3));
-        Task::GenEventManager::EventPtr d(new EventD(4));
-        Task::GenEventManager::EventPtr e(new EventE(5));
+        Task::GenEventManager::EventPtr a(whichevent==0
+                                          ? static_cast<Task::Event*>(new EventA(1))
+                                          : (whichevent==1
+                                             ? static_cast<Task::Event*>(new EventB(1))
+                                             : (whichevent==2
+                                                ? static_cast<Task::Event*>(new EventC(1))
+                                                : (whichevent==3
+                                                   ? static_cast<Task::Event*>(new EventD(1))
+                                                   : static_cast<Task::Event*>(new EventE(1))))));
+        Task::GenEventManager::EventPtr b(whichevent==1
+                                          ? static_cast<Task::Event*>(new EventA(2))
+                                          : static_cast<Task::Event*>(new EventB(3)));
+        Task::GenEventManager::EventPtr c(whichevent==2
+                                          ? static_cast<Task::Event*>(new EventA(3))
+                                          : static_cast<Task::Event*>(new EventC(3)));
+        Task::GenEventManager::EventPtr d(whichevent==3
+                                          ? static_cast<Task::Event*>(new EventA(4))
+                                          : static_cast<Task::Event*>(new EventD(4)));
+        Task::GenEventManager::EventPtr e(whichevent==4
+                                          ? static_cast<Task::Event*>(new EventA(5))
+                                          : static_cast<Task::Event*>(new EventE(5)));
+        Task::SubscriptionId a_removeId,bid,cid,did,eid;
         mManager->subscribe(a->getId(),
                             boost::bind(&EventSystemTestSuite::oneShotTest,this,_1));
-        mManager->subscribe(b->getId(),
+        bid=mManager->subscribeId(b->getId(),
                             boost::bind(&EventSystemTestSuite::doNotCall,this,_1));
-        mManager->subscribe(c->getId(),
+        cid=mManager->subscribeId(c->getId(),
                             boost::bind(&EventSystemTestSuite::doNotCall,this,_1));
-        mManager->subscribe(d->getId(),
+        did=mManager->subscribeId(d->getId(),
                             boost::bind(&EventSystemTestSuite::doNotCall,this,_1));
-        mManager->subscribe(e->getId(),
+        eid=mManager->subscribeId(e->getId(),
                             boost::bind(&EventSystemTestSuite::doNotCall,this,_1));
+        //fire the first handler
+        mManager->fire(a);
+        //this second firing should be a noop
         mManager->fire(a);
     
-	mManager->temporary_processEventQueue(Task::AbsTime::null()); // FIXME: This function will change.
-	// call twice just to make sure.
-	mManager->temporary_processEventQueue(Task::AbsTime::null());
-
-	int handler_A_should_have_been_called_exactly_once = mCount;
+        mManager->temporary_processEventQueue(Task::AbsTime::null()); // FIXME: This function will change.
+        // call twice just to make sure.
+        mManager->temporary_processEventQueue(Task::AbsTime::null());
+        
+        int handler_A_should_have_been_called_exactly_once = mCount;
         TS_ASSERT_EQUALS(handler_A_should_have_been_called_exactly_once,1);
         TS_ASSERT(mFail==false&&"Wrong handler got the signal");            
-
-    int a_removeId = mManager->subscribeId(a->getId(),
-                            boost::bind(&EventSystemTestSuite::oneShotTest,this,_1));
-	mManager->unsubscribe(a_removeId);
-	mCount = 0;
-	mManager->fire(a);
-	mManager->fire(b);
-	mManager->fire(c);
-	mManager->fire(d);
-	mManager->fire(e);
-	
-	mManager->temporary_processEventQueue(Task::AbsTime::null());
-
-        TS_ASSERT(mFail==true&&"Not enough handlers passed");
-	int unsubscribe_should_be_0 = mCount;
+        
+        mManager->unsubscribe(bid);
+        mManager->unsubscribe(cid);
+        mManager->unsubscribe(did);
+        mManager->unsubscribe(eid);
+        mCount = 0;
+        a_removeId = mManager->subscribeId(a->getId(),
+                                                          boost::bind(&EventSystemTestSuite::oneShotTest,this,_1));
+        mManager->unsubscribe(a_removeId);
+        mManager->fire(a);
+        mManager->fire(b);
+        mManager->fire(c);
+        mManager->fire(d);
+        mManager->fire(e);
+        
+        mManager->temporary_processEventQueue(Task::AbsTime::null());
+        mManager->temporary_processEventQueue(Task::AbsTime::null());
+        mManager->temporary_processEventQueue(Task::AbsTime::null());
+        mManager->temporary_processEventQueue(Task::AbsTime::null());
+        mManager->temporary_processEventQueue(Task::AbsTime::null());
+        
+        int unsubscribe_should_be_0 = mCount;
         TS_ASSERT_EQUALS(unsubscribe_should_be_0, 0);
-  }
+    }
+    void testDeliveryA( void ) {
+        deliveryABCDE(0);
+    }
+
+    void testDeliveryB( void ) {
+        deliveryABCDE(1);
+    }
+
+    void testDeliveryC( void ) {
+        deliveryABCDE(2);
+    }
+
+    void testDeliveryD( void ) {
+        deliveryABCDE(3);
+    }
+
+    void testDeliveryE( void ) {
+        deliveryABCDE(4);
+    }
 };
 
