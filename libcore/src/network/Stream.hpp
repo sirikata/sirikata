a9069065040a64fa142c7487476c297d1fd09d8a
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2009-02-22 16:04:10 -0800

    Overarching documentation on class interfaces

--- libcore/src/network/Stream.hpp
+++ libcore/src/network/Stream.hpp
@@ -42,39 +42,44 @@ typedef std::vector<uint8> Chunk;
  * As soon as connect is called, streams may be cloned or data may be sent across them
  * When the stream finds out whether a connection attempt failed or succeeded, 
  * it will call the connectionCallback
- * When the other side calls clone() the first side will receive a substreamCallback
+ * When the other side calls clone() the first side will receive a substreamCallback with the callback it provided on connect() or listen()
+ * Once a stream is clone()d, it may outlast the parent stream and may receive further substream callbacks on the original substreamCallback provided
+
  * 
- * -Unresolved:
- * Should a stream that clone()d a substream and then closes force the substream to close
- *  + this would help build compatibility with structured streams
- *  + It would become easier to shut the whole application down by closing the toplevel streams
- *  - This makes synchronization difficult, because the other ops can pretty much use os-stream level synchronization on the socket itself
- *  - It's unclear where, aside from shutdown, this sort of recursive closure helps out
- *  or we could do the worst of both worlds: do a best effort check in debug mode and call it 'undefined' behavior to use a closed substream
  */
 class Stream {
 protected:
     Stream(){}
 public:
+    /**
+     * This class stores up to 30 bits of data in a 4 byte memory slot. It may be serialized to between 1 and 4 bytes
+     * This class is useful for defining unique StreamIDs as well as other lowlevel data like packet lengths
+     */
     class uint30{
         uint32 mID;
     public:
+        ///The maximum length that any uint30 will be serialized out to
         enum MaxSerializedLengthConstants{
+            ///The maximum length that any uint30 will be serialized out to
             MAX_SERIALIZED_LENGTH=4
         };
+        ///True if this uint32 is odd
         bool odd() const{
             return (mID&1);
         }
+        ///Returns the inmemory value of this uint30
         uint32 read() const{
             return mID;
         }
-        //unserializes a streamID into a buffer of size at least 8. Caller should check return value to see if size is too small
+        //unserializes a streamID into a buffer of size at least 8. Caller should check return value to see how much space actually used
         unsigned int serialize(uint8 *destination, unsigned int maxsize)const;
-        //unserializes a streamID into a buffer where the size is at least size...puts bytes consumed into size variable
+        //unserializes a streamID into a buffer where the size is at least size...puts bytes consumed into size variable returns false if size too small
         bool unserialize(const uint8 *src, unsigned int &size);
-        uint30(){//control packet
+        ///Construct an integer filled with 0 sized value. Will be used for control packets for StreamIDs
+        uint30(){
             mID=0;
         }
+        ///Construct a uint30 based on given integer value
         uint30(unsigned int id){
             assert(id<(1<<30));
             mID=id;
@@ -88,49 +93,73 @@ public:
 		}
 		/// Hasher functor to be used in a hash_map.
 		struct Hasher {
+            ///returns a hash of the integer value
 			size_t operator() (const uint30 &id) const{
 				return std::tr1::hash<unsigned int>()(id.mID);
 			}
 		};
-
+        ///Total ordering of uint30
         bool operator == (const uint30&other)const {
             return mID==other.mID;
         }
+        ///Total ordering of uint30
         bool operator != (const uint30&other)const {
             return mID!=other.mID;
         }
+        ///Total ordering of uint30
         bool operator < (const uint30&other)const {
             return mID<other.mID;
         }
     };
+    ///The number of live streams on a single connection must fit into 30 bits. Streams will be reused when they are shutdown
     typedef uint30 StreamID ;
+    ///Callback codes indicating whether the socket has connected, had a connection rejected or got a sudden disconnection
     enum ConnectionStatus {
         Connected,
         ConnectionFailed,
         Disconnected
     };
+    ///Codes indicating if packet sending should be reliable or not,and in order or not
     enum Reliability {
         Unreliable,
         ReliableUnordered,
         Reliable
     };
+    ///Callback type for when a connection happens (or doesn't) Callees will receive a ConnectionStatus code indicating connection successful, rejected or a later disconnection event
     typedef std::tr1::function<void(ConnectionStatus,const std::string&reason)> ConnectionCallback;
+    ///Callback type for when a full chunk of bytes are waiting on the stream
     typedef std::tr1::function<void(const Chunk&)> BytesReceivedCallback;
+    /**
+     *  This class is passed into any newSubstreamCallback functions so they may 
+     *  immediately setup callbacks for connetion events and possibly start sending immediate responses.     
+     */
     class SetCallbacks{
     public:
         virtual ~SetCallbacks(){}
+        /**
+         * Function to be called from within a SubstreamCallback to set the callback functions 
+         * of a newly cloned or received stream. This allows bytes to be immediately sent off
+         */
         virtual void operator()(const Stream::ConnectionCallback &connectionCallback,
                                 const Stream::BytesReceivedCallback &bytesReceivedCallback)=0;
     };
     /**
      * The substreamCallback must call SetCallbacks' operator() to activate the stream
+     * Then it may send bytes asap
      * it will get called with NULL as the last stream shuts down
      */
     typedef std::tr1::function<void(Stream*,SetCallbacks&)> SubstreamCallback;
+    ///Simple example function to ignore incoming streams. This is used when a socket is shutting down, or for convenience
     static void ignoreSubstreamCallback(Stream * stream, SetCallbacks&);
+    ///Simple example function to ignore connection requests
     static void ignoreConnectionStatus(ConnectionStatus status,const std::string&reason);
+    ///Simple example function to ignore incoming bytes on a connection
     static void ignoreBytesReceived(const Chunk&);
-    ///subclasses will expose these methods with similar arguments + protocol specific args
+    /**
+     * Will attempt to connect to the given provided address, specifying all callbacks for the first successful stream
+     * The stream is immediately active and may have bytes sent on it immediately. 
+     * A connectionCallback will be called as soon as connection has succeeded or failed
+     */
     virtual void connect(
         const Address& addy,
         const SubstreamCallback &substreamCallback,
@@ -146,7 +175,7 @@ public:
     
     ///Send a chunk of data to the receiver
     virtual void send(const Chunk&data,Reliability)=0;
-    ///close this stream: if it is the last stream, close the connection (unresolved: what if this stream has children streams--do those forcably close?)
+    ///close this stream: if it is the last stream, close the connection as well
     virtual void close()=0;
     virtual ~Stream(){};
 };
--- libcore/src/network/TCPStream.hpp
+++ libcore/src/network/TCPStream.hpp
@@ -36,20 +36,76 @@
 namespace Sirikata { namespace Network {
 class MultiplexedSocket;
 
+
+/**
+ * This is a particular example implementation of the Stream interface sitting atop TCP.
+ * The protocol specified in the Structured streams paper http://pdos.csail.mit.edu/uia/sst/
+ * May be used as another implementation of the Stream interface. This is provided as the 
+ * default implementation as it supports a wide range of network devices and is not subject to
+ * prejudice against UDP connections which we routinely see on the net (my home system gets 10 
+ * second lag on UDP connections just due to ISP policy combating P2P networks)
+ *
+ * The protocol is as follows:
+ *
+ * --Connection--
+ * To connect to a remote host, the client opens a small number (N) of TCP sockets to that host
+ * and sends the version string (currently SSTTCP ), an ascii 2 digit representation of N (in this case '03')
+ * and a 16 byte unique ID used to pair up connections and possibly in the future for encryption
+ * The other side respinds in turn on each connection with a similar handshake (though the 16 bytes may be different)
+ * now the connection is online. The remote host may immediately follow the handshake with live packets and 
+ * the other side may respond as soon as it receives the remote hosts handshake response
+ *
+ * --Live Phase--
+ * The first live stream has StreamID of 1. New streams coming from listener must have even StreamID's and new 
+ * streams coming from connector must have odd stream ID's.
+ * To indicate a new substream, simply send a packet with a new stream ID
+ *
+ * --Packet Format--
+ * All packets are prepended by a variable length int30 length and int30 StreamID and then the bytestream of data
+ * The length value includes the length of the variable-length StreamID. 
+ * the int30 format is as follows
+ *      if the highest value bit in the first byte is 0, the other 7 bits represent numbers 0-128
+ *      if the highest value bit in the first byte is 1, and the highest value bit in the second byte is 0
+ *            the first 7 bits represent the 0-127 remainder and the first 7 bits in the second byte represent
+ *            the 0-128 significant 7 bit digit valued between 0 and 16256
+ *      if the highest value bit in the first and second bytes are both 1
+ *            the value between 0 and 16383 above is added to 16384*the third byte added to 16384*256*the fourth byte
+ *
+ * --Shutting down a Stream--
+ * If either side decides to close a stream they must send a control packet (which is the implicit stream with
+ * StreamID = default int30, which serializes to a single byte=0) 
+ * and then a control code which must be a byte equal to 1, and finally a variable length StreamID indicating which stream to close
+ * This control packet must be broadcast on every related open TCP socket connection
+ * This party may not reuse the streamID (given parity match) until it receives control packets with control code equal to 2 (close Ack) on all sockets.
+ * The other side must keep the bargain and send the control packet with control code 2 on all sockets to allow ID reuse.
+ * 
+ * If all streams are shut down, the sockets may be deactivated
+ * If the socket disconnects due to error, then Disconnect callbacks must be called
+ */
 class TCPStream:public Stream {
 public:
     class Callbacks;
 private:
     friend class MultiplexedSocket;
+    ///The low level boost::asio io service handle
     IOService* mIO;
+    ///the shared pointer to the communal sending connection
     std::tr1::shared_ptr<MultiplexedSocket> mSocket;
+    ///A function to add callbacks to this particular stream, called by the relevant TCPSetCallbacks function inheriting from SetCallbacks
     void addCallbacks(Callbacks*);
+    ///The streamID that must be prepended to the data within any packet sent and all received packets for this Stream
     StreamID mID;
+    ///A bit flag indicating that the socket is being shut down and no further sends may proceed
     static const int SendStatusClosing;
     ///incremented while sending: or'd in SendStatusClosing when close function triggered so no further packets will be sent using old ID.
     AtomicValue<int>mSendStatus;
 public:
+    ///Returns the active stream ID
     StreamID getID()const {return mID;}
+    /**
+     * A pair of callbacks related to a stream. Used for a target of a map type
+     * Holds the callback to receive for a connection and the bytes received callback
+     */
     class Callbacks:public Noncopyable {        
     public:
         Stream::ConnectionCallback mConnectionCallback;
@@ -59,22 +115,25 @@ public:
             mConnectionCallback(connectionCallback),mBytesReceivedCallback(bytesReceivedCallback){
         }
     };
+    ///Constructor which leaves socket in a disconnection state, prepared for a connect() or a clone()
     TCPStream(IOService&);
+    ///Constructor which brings the socket up to speed in a completely connected state, prepped with a StreamID and communal link pointer
     TCPStream(const std::tr1::shared_ptr<MultiplexedSocket> &shared_socket, const Stream::StreamID&);
-public:
+    ///Implementation of send interface
     virtual void send(const Chunk&data,Reliability);
+    ///Implementation of connect interface
     virtual void connect(
         const Address& addy,
         const SubstreamCallback &substreamCallback,
         const ConnectionCallback &connectionCallback,
         const BytesReceivedCallback&chunkReceivedCallback);
     ///Creates a stream of the same type as this stream, with the same IO factory
-    Stream* factory();
+    virtual Stream* factory();
     ///Creates a new substream on this connection
     virtual bool cloneFrom(Stream*,
         const ConnectionCallback &connectionCallback,
         const BytesReceivedCallback&chunkReceivedCallback);
-
+    //Shuts down the socket, allowing StreamID to be reused and opposing stream to get disconnection callback
     virtual void close();
 };
 } }
