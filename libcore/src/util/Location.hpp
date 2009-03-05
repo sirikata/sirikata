44c0312568d6b8733fb7730636d951d28c86b3b9
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2009-03-05 00:01:50 -0800

    Added Location (position+orientation+speed) class

---
+++ libcore/src/util/Location.hpp
@@ -0,0 +1,102 @@
+/*  Sirikata Utilities -- Math Library
+ *  Location.hpp
+ *
+ *  Copyright (c) 2009, Daniel Reiter Horn
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of Sirikata nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _LOCATION_HPP_
+#define _LOCATION_HPP_
+
+#include "Quaternion.hpp"
+
+namespace Sirikata {
+
+class Location: protected Vector3<double> {
+    Vector3<float> mVelocity;
+    Quaternion mOrientation;
+    Vector3<float> mAxisOfRotation;
+    float mAngularSpeed;
+public:
+    Location(){}
+    Location(const Vector3<double>&position, 
+             const Quaternion&orientation, 
+             const Vector3<float> &velocity, 
+             const Vector3<float> angularVelocityAxis, 
+             float angularVelocityRadians):mVelocity(velocity),mOrientation(orientation),mAxisOfRotation(angularVelocityAxis), mAngularSpeed(angularVelocityRadians) {
+        x=position.x;
+        y=position.y;
+        z=position.z;
+    }
+    const Vector3<double>&getPosition()const {
+        return *this;
+    }
+    void setPosition(const Vector3<double>& position) {
+        x=position.x;
+        y=position.y;
+        z=position.z;
+    }
+    const Vector3<float>&getVelocity()const {
+        return mVelocity;
+    }
+    void setVelocity(const Vector3<float> velocity) {
+        mVelocity=velocity;
+    } 
+    const Quaternion&getOrientation() const{
+        return mOrientation;
+    }
+    void setOrientation(const Quaternion&orientation) {
+        mOrientation=orientation;
+    }
+    const Vector3<float>&getAxisOfRotation() const {
+        return mAxisOfRotation;
+    }
+    void setAxisOfRotation(const Vector3<float>&axis) {
+        mAxisOfRotation=axis;
+    }
+    float getAngularSpeed() const{
+        return mAngularSpeed;
+    }
+    void setAngularSpeed(float radianspersecond) {
+        mAngularSpeed=radianspersecond;
+    }
+    void addAngularRotation(const Vector3<float> &axis, float radianspersecond) {
+        mAxisOfRotation=mAxisOfRotation*mAngularSpeed+axis*radianspersecond;
+        mAngularSpeed=mAxisOfRotation.length();
+        mAxisOfRotation/=mAngularSpeed;
+    }
+    template<class TimeDuration> Location predict(const TimeDuration&dt)const {
+        return Locaton(getPosition()+getVelocity()*(double)dt,
+                       getOrientation()*Quaternion(getAxisOfRotation(),
+                                                   getAngularSpeed()*(double)dt),
+                       getVelocity(),
+                       getAxisOfRotation(),
+                       getAngularSpeed());
+    }
+};
+}
+#endif
