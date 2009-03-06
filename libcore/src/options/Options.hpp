e5c5b66c358b782bd67ec56e20fecd6ea56831f1
Patrick Horn <patrick.horn@gmail.com>
2009-03-05 17:48:24 -0800

    Fixed doxygen error.

--- libcore/src/options/Options.hpp
+++ libcore/src/options/Options.hpp
@@ -106,11 +106,11 @@ public:
     }
     /**
      * Invoke an option, setting its default value with a particular lexiccal cast function, a description and optionally a pointer to set to the result
-     * \parameter option is the name of the option in the config or text files
-     * \parameter defaultValue is the string representation of the default value as if a user typed it in
-     * \parameter type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
-     * \parameter description is the textual description for the user when looking through the command line help
-     * \parameter pointer holds a pointer to an OptionValue that will get set to the newly constructed class
+     * \param option is the name of the option in the config or text files
+     * \param defaultValue is the string representation of the default value as if a user typed it in
+     * \param type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
+     * \param description is the textual description for the user when looking through the command line help
+     * \param pointer holds a pointer to an OptionValue that will get set to the newly constructed class
      */
     template<class T>OptionValue(const char*option, const std::string&defaultValue, T type, const char*description, OptionValue**pointer=NULL){
         mParser=std::tr1::function<Any(std::string)>(&T::lexical_cast);
@@ -123,12 +123,12 @@ public:
     }
     /**
      * Invoke an option, setting its default value with a particular lexiccal cast function, a description and optionally a pointer to set to the result
-     * \parameter option is the name of the option in the config or text files
-     * \parameter defaultValue is the string representation of the default value as if a user typed it in
-     * \parameter type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
-     * \parameter description is the textual description for the user when looking through the command line help
-     * \parameter changeFunction is the function that will be invoked if someone atomically changes the OptionValue type by invoking =
-     * \parameter pointer holds a pointer to an OptionValue that will get set to the newly constructed class
+     * \param option is the name of the option in the config or text files
+     * \param defaultValue is the string representation of the default value as if a user typed it in
+     * \param type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
+     * \param description is the textual description for the user when looking through the command line help
+     * \param changeFunction is the function that will be invoked if someone atomically changes the OptionValue type by invoking =
+     * \param pointer holds a pointer to an OptionValue that will get set to the newly constructed class
      */
     template<class T>OptionValue(const char* option, const std::string&defaultValue, T xtype, const char*description, std::tr1::function<void(const std::string&,Any,Any)>&changeFunction, OptionValue**pointer=NULL) {
         mParser=std::tr1::function<Any(std::string)>(&T::lexical_cast);
@@ -141,12 +141,12 @@ public:
     }
     /**
      * Invoke an option, setting its default value with a particular lexiccal cast function, a description and optionally a pointer to set to the result
-     * \parameter option is the name of the option in the config or text files
-     * \parameter defaultValue is the string representation of the default value as if a user typed it in
-     * \parameter type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
-     * \parameter description is the textual description for the user when looking through the command line help
-     * \parameter parser is the function that can convert a string to the Sirikata::Any of the appropriate type
-     * \parameter pointer holds a pointer to an OptionValue that will get set to the newly constructed class
+     * \param option is the name of the option in the config or text files
+     * \param defaultValue is the string representation of the default value as if a user typed it in
+     * \param type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
+     * \param description is the textual description for the user when looking through the command line help
+     * \param parser is the function that can convert a string to the Sirikata::Any of the appropriate type
+     * \param pointer holds a pointer to an OptionValue that will get set to the newly constructed class
      */
     OptionValue(const char* option, const std::string&defaultValue, const char *description, const std::tr1::function<Any(std::string)>& parser, OptionValue**pointer=NULL) {
         mParser=parser;
@@ -159,13 +159,13 @@ public:
     }
     /**
      * Invoke an option, setting its default value with a particular lexiccal cast function, a description and optionally a pointer to set to the result
-     * \parameter option is the name of the option in the config or text files
-     * \parameter defaultValue is the string representation of the default value as if a user typed it in
-     * \parameter type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
-     * \parameter description is the textual description for the user when looking through the command line help
-     * \parameter parser is the function that can convert a string to the Sirikata::Any of the appropriate type
-     * \parameter changeFunction is the function that will be invoked if someone atomically changes the OptionValue type by invoking =
-     * \parameter pointer holds a pointer to an OptionValue that will get set to the newly constructed class
+     * \param option is the name of the option in the config or text files
+     * \param defaultValue is the string representation of the default value as if a user typed it in
+     * \param type is a class that contains a static lexical_cast function that transforms a string into an Sirikata::Any of the appropirate type
+     * \param description is the textual description for the user when looking through the command line help
+     * \param parser is the function that can convert a string to the Sirikata::Any of the appropriate type
+     * \param changeFunction is the function that will be invoked if someone atomically changes the OptionValue type by invoking =
+     * \param pointer holds a pointer to an OptionValue that will get set to the newly constructed class
      */
     OptionValue(const char* option, const std::string&defaultValue, const char* description, const std::tr1::function<Any(std::string)>& parser,  const std::tr1::function<void(const std::string&, Any, Any)> &changeFunction, OptionValue**pointer=NULL) {
         mParser=parser;
