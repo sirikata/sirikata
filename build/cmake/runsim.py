b9ea8afe16f4d3a81fc327b9f5edf9af8f29c5b6
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2009-03-17 00:41:13 -0700

    Add support for bandwidth argument to runsim.py script.

--- build/cmake/runsim.py
+++ build/cmake/runsim.py
@@ -4,12 +4,13 @@ import math
 nservers=int(sys.argv[1]);
 duration=float(sys.argv[2]);
 flatness=float(sys.argv[3]);
-if len(sys.argv)>4:
-    waitadditional=sys.argv[4];
+bandwidth=float(sys.argv[4]);
+if len(sys.argv)>5:
+    waitadditional=sys.argv[5];
 else:
     waitadditional="8"
-if len(sys.argv)>5:
-    shouldcapbandwidth=sys.argv[5];
+if len(sys.argv)>6:
+    shouldcapbandwidth=sys.argv[6];
 else:
     shouldcapbandwidth="false"
 
@@ -38,5 +39,5 @@ for i in range(0,xservers):
         if i+1==xservers and j+1==yservers:
             shouldwait=os.P_WAIT
         #in the future you want to change this to ssh to the appropriate machine and run the command below as an argument to ssh
-        os.spawnl(shouldwait,"./cbr","./cbr","--id="+str(sid),"--layout=<"+str(xservers)+','+str(yservers)+",1>","--duration="+str(duration)+'s',"--serverips="+serveripfile,"--flatness="+str(flatness),"--capexcessbandwidth="+shouldcapbandwidth,"--wait-until="+datestr+"","--wait-additional="+waitadditional);
+        os.spawnl(shouldwait,"./cbr","./cbr","--id="+str(sid),"--layout=<"+str(xservers)+','+str(yservers)+",1>","--duration="+str(duration)+'s',"--serverips="+serveripfile,"--flatness="+str(flatness),"--bandwidth="+str(bandwidth),"--capexcessbandwidth="+shouldcapbandwidth,"--wait-until="+datestr+"","--wait-additional="+waitadditional);
     
