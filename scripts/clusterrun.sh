7dbce7fb12e00b9451cce3e718580bab2c721eae
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2009-04-25 02:31:29 -0700

    1.1 meters gonk,gonk: run all bandwidth stats and coalesce them into a labeled directory

--- scripts/clusterrun.sh
+++ scripts/clusterrun.sh
@@ -13,5 +13,29 @@ export numnum=9
 export duration=100s
 export datetime=`date +"%Y-%m-%d %H:%M:%S%z"`
 DISPLAY=:0.0 ../../scripts/runallx.sh "cd cobbler/build/cmake&&python ../../scripts/runoneserversim.py $numnum --duration=$duration --bandwidth=$bandwidth --wait-additional=6 --flatness=.001 --capexcessbandwidth=false --object.queue=fairfifo --wait-until=$datetime "
+scp $CLUSTERUSER@meru00:cobbler/build/cmake/trace-0001.txt .
+scp $CLUSTERUSER@meru01:cobbler/build/cmake/trace-0002.txt .
+scp $CLUSTERUSER@meru02:cobbler/build/cmake/trace-0003.txt .
+scp $CLUSTERUSER@meru03:cobbler/build/cmake/trace-0004.txt .
+scp $CLUSTERUSER@meru04:cobbler/build/cmake/trace-0005.txt .
+
+scp $CLUSTERUSER@meru00:cobbler/build/cmake/sync-0001.txt .
+scp $CLUSTERUSER@meru01:cobbler/build/cmake/sync-0002.txt .
+scp $CLUSTERUSER@meru02:cobbler/build/cmake/sync-0003.txt .
+scp $CLUSTERUSER@meru03:cobbler/build/cmake/sync-0004.txt .
+scp $CLUSTERUSER@meru04:cobbler/build/cmake/sync-0005.txt .
+
+./cbr --id=1 --layout="<$num,$num,1>" --duration=$duration --bandwidth=$bandwidth  --flatness=.001 --capexcessbandwidth=false --object.queue=fairfifo --serverips=serverip-$num-$num.txt --analysis.windowed-bandwidth=packet 
+./cbr --id=1 --layout="<$num,$num,1>" --duration=$duration --bandwidth=$bandwidth  --flatness=.001 --capexcessbandwidth=false --object.queue=fairfifo --serverips=serverip-$num-$num.txt --analysis.windowed-bandwidth=datagram
+python ../../scripts/graph_windowed_bandwidth.py windowed_bandwidth_packet_send.dat &
+python ../../scripts/graph_windowed_bandwidth.py windowed_bandwidth_packet_receive.dat &
+python ../../scripts/graph_windowed_bandwidth.py windowed_bandwidth_datagram_send.dat &
+python ../../scripts/graph_windowed_bandwidth.py windowed_bandwidth_datagram_receive.dat &
+wait
+export datadir="d-$numnum-$bandwidth-$flatness-"`date +%H-%M-%S-%Y%m%d`
+mkdir $datadir
+mv trace-* $datadir
+mv sync-* $datadir
+mv windowed_bandwidth* $datadir
 
 echo done
