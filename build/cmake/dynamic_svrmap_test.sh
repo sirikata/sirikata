3520f44804d457b5c807f83e3db5b72506156b2b
Tahir Azim <tahir@indus.(none)>
2009-05-06 17:03:30 -0700

    Add commands to generate bandwidth graphs

--- build/cmake/dynamic_svrmap_test.sh
+++ build/cmake/dynamic_svrmap_test.sh
@@ -7,5 +7,9 @@ export numnum=9
 python ../../scripts/dynamic_svrmap_runsim.py $numnum --duration=100s --receive-bandwidth=$bandwidth --send-bandwidth=$bandwidth --wait-additional=6 --flatness=.001 --capexcessbandwidth=false --object.queue=fairfifo
 #./cbr --id=1 --serverips=serverip-3-3.txt --duration 100s --analysis.loc true >> loc_error.txt
 ./cbr --id=1 "--layout=<$num,$num,1>" --serverips=serverip-$num-$num.txt --duration 100s --analysis.locvis=1 #&> windowed_bandwidth.txt
-#    ./cbr --id=1 "--layout=<2,2,1>" --serverips=serverip-3-3.txt --duration 100s --analysis.bandwidth true
+
+./cbr  "--layout=<4,4,1>" --serverips=serverip-$num-$num.txt --duration 100s --analysis.windowed-bandwidth packet
+
+python ../../scripts/graph_windowed_bandwidth.py windowed_bandwidth_packet_send.dat
+
 #done
