e8938d869c87a84ef1824d982d1ab036c620396b
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2009-05-11 00:26:03 -0700

    Rearrange cluster_sim to have an actual class for running it, with an options class.  Needs better command line interface.

--- scripts/cluster_sim.py
+++ scripts/cluster_sim.py
@@ -8,60 +8,98 @@ from cluster_run import ClusterRun
 from cluster_run import ClusterDeploymentRun
 from cluster_scp import ClusterSCP
 
+
 # User parameters
-layout_x = 3 # FIXME
-layout_y = 3 # FIXME
+class ClusterSimSettings:
+    def __init__(self, x, y):
+        self.layout_x = x
+        self.layout_y = y
+        self.duration = '100s'
+        self.tx_bandwidth = 10000
+        self.rx_bandwidth = 10000
+        self.flatness = .001
+        self.num_objects = 100
+        self.server_queue = 'fair'
+        self.object_queue = 'fairfifo'
 
-duration = '100s'
-tx_bandwidth = 10000
-rx_bandwidth = 10000
-flatness = .001
-num_objects = 100
-server_queue = 'fair'
-object_queue = 'fairfifo'
+    def layout(self):
+        return "<" + str(self.layout_x) + "," + str(self.layout_y) + ",1>"
+
+class ClusterSim:
+    def __init__(self, config, x, y):
+        self.config = config
+        self.settings = ClusterSimSettings(x, y)
+
+    def num_servers(self):
+        return self.settings.layout_x * self.settings.layout_y
+
+    def ip_file(self):
+        return "serverip-" + str(self.settings.layout_x) + '-' + str(self.settings.layout_y) + '.txt'
+
+    def build_dir(self):
+        return self.config.code_dir + "/build/cmake/"
+
+    def scripts_dir(self):
+        return self.config.code_dir + "/scripts/"
+
+    def run(self):
+        self.config.generate_deployment(self.num_servers())
+        self.generate_ip_file()
+        self.run_instances()
+        self.retrieve_data()
+        self.run_analysis()
+
+    def generate_ip_file(self):
+        # Generate the serverip file, copy it to all nodes
+        serveripfile = self.ip_file()
+        fp = open(serveripfile,'w')
+        port = 6666
+        server_index = 0;
+        for i in range(0, self.settings.layout_x):
+            for j in range(0, self.settings.layout_y):
+                fp.write(self.config.deploy_nodes[server_index].node+":"+str(port)+'\n')
+                port += 1
+                server_index += 1
+        fp.close()
+        ClusterSCP(self.config, [serveripfile, "remote:" + self.build_dir()])
+
+    def run_instances(self):
+        # Construct the command lines to do the actual run and dispatch them
+        cmd = "cd " + self.build_dir() + "; "
+        cmd += "./cbr "
+        cmd += "--id=%(node)d"
+        cmd += "\"--layout=" + self.settings.layout() + "\" "
+        cmd += "--serverips=" + self.ip_file() + " "
+        cmd += "--duration=" + self.settings.duration + " "
+        cmd += "--send-bandwidth=" + str(self.settings.tx_bandwidth) + " "
+        cmd += "--receive-bandwidth=" + str(self.settings.rx_bandwidth) + " "
+        cmd += "--wait-additional=6 "
+        cmd += "--flatness=" + str(self.settings.flatness) + " "
+        cmd += "--capexcessbandwidth=false "
+        cmd += "--objects=" + str(self.settings.num_objects) + " "
+        cmd += "--server.queue=" + self.settings.server_queue + " "
+        cmd += "--object.queue=" + self.settings.object_queue + " "
+        ClusterDeploymentRun(self.config, cmd)
+
+    def retrieve_data(self):
+        # Copy the trace and sync data back here
+        trace_file_pattern = "remote:" + self.build_dir() + "trace-%(node)04d.txt"
+        sync_file_pattern = "remote:" + self.build_dir() + "sync-%(node)04d.txt"
+        ClusterSCP(self.config, [trace_file_pattern, sync_file_pattern, "."])
+
+    def run_analysis(self):
+        # Run analysis
+        subprocess.call(['../build/cmake/cbr', '--id=1', "--layout=" + self.settings.layout(), "--serverips=" + self.ip_file(), "--duration=" + self.settings.duration, '--analysis.windowed-bandwidth=packet', '--analysis.windowed-bandwidth.rate=100ms'])
+        subprocess.call(['../build/cmake/cbr', '--id=1', "--layout=" + self.settings.layout(), "--serverips=" + self.ip_file(), "--duration=" + self.settings.duration, '--analysis.windowed-bandwidth=datagram', '--analysis.windowed-bandwidth.rate=100ms'])
+
+        subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_packet_send.dat'])
+        subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_packet_receive.dat'])
+        subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_datagram_send.dat'])
+        subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_datagram_receive.dat'])
 
 
-nservers = layout_x * layout_y
 
-cc = ClusterConfig()
-cc.generate_deployment(nservers)
-
-#ClusterRun(cc, "cd ewencp/cbr.git/build/cmake; cmake .; make clean; make -j2")
-
-
-# Generate the serverip file, copy it to all nodes
-serveripfile = "serverip-" + str(layout_x) + '-' + str(layout_y) + '.txt'
-fp = open(serveripfile,'w')
-port = 6666
-server_index = 0;
-for i in range(0, layout_x):
-    for j in range(0, layout_y):
-        fp.write(cc.deploy_nodes[server_index].node+":"+str(port)+'\n')
-        port += 1
-        server_index += 1
-fp.close()
-ClusterSCP(cc, [serveripfile, "remote:ewencp/cbr.git/build/cmake/"])
-
-
-# Construct the command lines to do the actual run and dispatch them
-cmd_fmt = "cd ewencp/cbr.git/build/cmake; "
-cmd_fmt += "./cbr "
-cmd_fmt += "\"--layout=<" + str(layout_x)+","+str(layout_y)+",1>\" "
-cmd_fmt += "--serverips=" + serveripfile + " "
-cmd_fmt += " --duration=%(duration)s --send-bandwidth=%(sbandwidth)d --receive-bandwidth=%(rbandwidth)d --wait-additional=6 --flatness=%(flatness)f --capexcessbandwidth=false --objects=%(numobjects)d --server.queue=%(squeue)s --object.queue=%(oqueue)s "
-cmd = cmd_fmt % { 'numsq' : nservers, 'duration' : duration, 'sbandwidth' : tx_bandwidth, 'rbandwidth' : rx_bandwidth, 'flatness' : flatness, 'numobjects' : num_objects, 'squeue' : server_queue, 'oqueue' : object_queue}
-cmd = cmd + "--id=%(node)d "
-print cmd
-ClusterDeploymentRun(cc, cmd)
-
-# Copy the trace and sync data back here
-ClusterSCP(cc, ["remote:ewencp/cbr.git/build/cmake/trace-%(node)04d.txt", "remote:ewencp/cbr.git/build/cmake/sync-%(node)04d.txt", "."])
-
-# Run analysis
-subprocess.call(['../build/cmake/cbr', '--id=1', "--layout=<" + str(layout_x)+","+str(layout_y)+",1>", "--serverips=" + serveripfile, "--duration=100s", '--analysis.windowed-bandwidth=packet', '--analysis.windowed-bandwidth.rate=100ms'])
-subprocess.call(['../build/cmake/cbr', '--id=1', "--layout=<" + str(layout_x)+","+str(layout_y)+",1>", "--serverips=" + serveripfile, "--duration=100s", '--analysis.windowed-bandwidth=datagram', '--analysis.windowed-bandwidth.rate=100ms'])
-
-subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_packet_send.dat'])
-subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_packet_receive.dat'])
-subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_datagram_send.dat'])
-subprocess.call(['python', './graph_windowed_bandwidth.py', 'windowed_bandwidth_datagram_receive.dat'])
+if __name__ == "__main__":
+    cc = ClusterConfig()
+    cluster_sim = ClusterSim(cc, 2, 2)
+    cluster_sim.run()
