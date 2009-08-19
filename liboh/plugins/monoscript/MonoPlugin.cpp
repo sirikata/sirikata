bf5c5e164bb060fcbb8256a9c92740589887da02
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2009-08-18 19:06:27 -0700

    now should run in xcode without moving Sirikata.Runtime.dll into Debug or Release (please delete older copies of those .dll's as the newer ones are necessary for full compat

--- liboh/plugins/monoscript/MonoPlugin.cpp
+++ liboh/plugins/monoscript/MonoPlugin.cpp
@@ -54,6 +54,7 @@ bool loadDependencyAssembly(Mono::MonoSystem*mono_system, const Sirikata::String
         ||mono_system->loadAssembly(assembly,"../../dependencies/Cecil")
         ||mono_system->loadAssembly(assembly,".")
         ||mono_system->loadAssembly(assembly,"bin")
+        ||mono_system->loadAssembly(assembly,"..")
         ||mono_system->loadAssembly(assembly,"dependencies/IronPython")
         ||mono_system->loadAssembly(assembly,"dependencies/Cecil");
 }
@@ -62,7 +63,9 @@ bool loadCustomAssembly(Mono::MonoSystem*mono_system, const Sirikata::String&ass
             mono_system->loadAssembly(assembly,".")
             ||mono_system->loadAssembly(assembly,"bin")
             ||mono_system->loadAssembly(assembly,"Release")
-            ||mono_system->loadAssembly(assembly,"Debug");
+            ||mono_system->loadAssembly(assembly,"Debug")
+            ||mono_system->loadAssembly(assembly,"..");//for XCode
+
 }
 
 SIRIKATA_PLUGIN_EXPORT_C void init() {
