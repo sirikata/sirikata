94f379f443e4998cabba06d25acd7bfaf2a3ead7
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2009-09-10 17:35:33 -0700

    Made SqrIntegral return reasonable values, reduced NCALLS to the sqrroot of the original guesstimate to get values with 3% error---made flatness actually control flatness, not inner cutoff values

--- src/SqrIntegral.cpp
+++ src/SqrIntegral.cpp
@@ -9,12 +9,19 @@ extern "C" {
 struct sqrParams{
     double constant_extent_rho;
     double constant_inner_speed_k;
+    double flatness;
 };
-double inverse_polynomial(double r) {
-    double logr=log(r);
-    return 1./(r*r*logr*logr*logr);
+double inverse_polynomial(double r, double r2) {
+    double logr=log(r+1);
+    return 1./(r2*logr*logr);
 }
-
+double bandwidth_bound_no_cutoff(double r, double r2, size_t dim, void *v_params) {
+    struct sqrParams*params=(struct sqrParams*)v_params;    
+    double flatness=params->flatness;
+    r*=flatness;
+    r2*=flatness*flatness;
+    return inverse_polynomial(r,r2);
+}
 double bandwidth_bound(double *x, size_t dim, void *v_params) {
     struct sqrParams*params=(struct sqrParams*)v_params;
     
@@ -23,10 +30,10 @@ double bandwidth_bound(double *x, size_t dim, void *v_params) {
     double r2=(x[2]-x[0])*(x[2]-x[0])+(x[3]-x[1])*(x[3]-x[1]);
     double r=sqrt(r2);
     if (r<=p) return k;
-    return inverse_polynomial(r);
+    return bandwidth_bound_no_cutoff(r,r2,dim,params);
 }
 }
-#define gsl_monte_plain_integratePRINT(F0, xl, xu, dim, NCALLS, r, s, res1, err1) (printf ("Integrating (%.0f %.0f)->(%.0f %.0f) to (%.0f %.0f)->(%.0f %.0f)\n",xl[0],xl[1],xu[0],xu[1],xl[2],xl[3],xu[2],xu[3]),gsl_monte_plain_integrate(F0, xl, xu, dim, NCALLS, r, s, res1, err1));                
+#define gsl_monte_plain_integratePRINT(F0, xl, xu, dim, NCALLS, r, s, res1, err1) (gsl_monte_plain_integrate(F0, xl, xu, dim, NCALLS, r, s, res1, err1)),printf ("Integrating (%.0f %.0f)->(%.0f %.0f) to (%.0f %.0f)->(%.0f %.0f)=%lf -+ %lf\n",xl[0],xl[1],xu[0],xu[1],xl[2],xl[3],xu[2],xu[3],*res1,*err1);                
 
 namespace CBR {
 SqrIntegral::SqrIntegral() {
@@ -34,28 +41,30 @@ SqrIntegral::SqrIntegral() {
 	gsl_rng *r = gsl_rng_alloc(gsl_rng_default);
     gsl_rng_r=r;
     gsl_monte_plane_state_s=s;
-
+    memset(cachedDimensions,0,sizeof(cachedDimensions));
     memset(cachedCutoff,0,sizeof(cachedCutoff));
     memset(cachedFlatness,0,sizeof(cachedFlatness));
     memset(infiniteIntegral,0,sizeof(infiniteIntegral));
 }
 double SqrIntegral::integrate(double cutoff, double flatness,const Vector3d&xymin, const Vector3d&xymax, const Vector3d &uvmin, const Vector3d &uvmax, double*error){
-    int NCALLS=50000;
+    int NCALLS=256;
     sqrParams params;
     params.constant_extent_rho=cutoff;
-    params.constant_inner_speed_k=flatness;
+    params.constant_inner_speed_k=1.0;
+    params.flatness=flatness;
+    params.constant_inner_speed_k=bandwidth_bound_no_cutoff(cutoff,cutoff*cutoff,4,&params);
     double xl[4]={xymin.x,xymin.y,uvmin.x,uvmin.y};
     double xu[4]={xymax.x,xymax.y,uvmax.x,uvmax.y};
     double res1=0;
     gsl_monte_function F0 = { &bandwidth_bound, 4, &params};        
 	gsl_monte_plain_integrate(&F0, xl, xu, 4, NCALLS, (gsl_rng*)gsl_rng_r, (gsl_monte_plain_state*)gsl_monte_plane_state_s, &res1, error);    
+    //printf ("%lf, %lf\n",res1,*error);
     return res1;
 }
 
 double SqrIntegral::computeWithGivenCutoff(int whichCutoff,const Vector3d&xymin, const Vector3d&xymax, const Vector3d &uvmin, const Vector3d &uvmax){
     double error=0.0;
-    double currentRegion=integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],xymin,xymax,xymin,xymax,&error);
-    double renormalization = (infiniteIntegral[whichCutoff]*(xymax.x-xymin.x)*(xymax.y-xymin.y)-currentRegion);
+    double renormalization = infiniteIntegral[whichCutoff]*(xymax.x-xymin.x)*(xymax.y-xymin.y);
     return integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],xymin,xymax,uvmin,uvmax,&error)/renormalization;
 }
 
@@ -63,20 +72,20 @@ double SqrIntegral::computeInfiniteIntegral(int whichCutoff) {
     double sum=0;
     double err_sum=0;
     int i;
-    for (i=1;i<1000;i*=2) {
+
+    double xmul=cachedDimensions[whichCutoff][0];
+    double ymul=cachedDimensions[whichCutoff][1];
+    for (i=1;i<10000;i*=2) {
         int j;
-        for ( j=1;j<1000;j*=2) {
+        for ( j=1;j<10000;j*=2) {
             double error=0.0;
-            sum+=integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],Vector3d(0,0,0),Vector3d(1,1,1),Vector3d(i,j,0),Vector3d(i*2,j*2,1),&error);
+            sum+=integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],Vector3d(0,0,0),Vector3d(xmul,ymul,1),Vector3d(i*xmul,j*ymul,0),Vector3d(i*2*xmul,j*2*ymul,1),&error);
             err_sum+=error;
         }
         double error =0.0;
-        sum+=integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],Vector3d(0,0,0),Vector3d(1,1,1),Vector3d(i,0,0),Vector3d(i*2,1,1),&error);
+        sum+=integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],Vector3d(0,0,0),Vector3d(xmul,ymul,1),Vector3d(i*xmul,0,0),Vector3d(i*2*xmul,ymul,1),&error);
         err_sum+=error;       
    }
-   double error =0.0;
-   sum+=integrate(cachedCutoff[whichCutoff],cachedFlatness[whichCutoff],Vector3d(0,0,0),Vector3d(1,1,1),Vector3d(0,0,0),Vector3d(1,1,1),&error);
-   err_sum+=error;
    if ((err_sum*err_sum)/(sum*sum)>.01) {
        fprintf(stderr,"Error is %f on absolute of %f\n",err_sum,sum);
    }
--- src/SqrIntegral.hpp
+++ src/SqrIntegral.hpp
@@ -4,10 +4,18 @@ namespace CBR {
 class SqrIntegral {
     enum {MAX_CACHE=2};
     double cachedCutoff[MAX_CACHE];
+    double cachedDimensions[MAX_CACHE][2];
     double cachedFlatness[MAX_CACHE];
     double infiniteIntegral[MAX_CACHE];
     void *gsl_rng_r;
     void *gsl_monte_plane_state_s;
+    static bool approxEqual(double a, double b) {
+        double eps=1.0e-6;
+        if (a>b) {
+            return a-b<eps;
+        }
+        return b-a<eps;
+    }
 public:
     SqrIntegral();
     typedef double result_type;
@@ -15,8 +23,9 @@ public:
     double computeWithGivenCutoff(int whichCutoff,const Vector3d&xymin, const Vector3d&xymax, const Vector3d &uvmin, const Vector3d &uvmax);
     double computeInfiniteIntegral(int whichCutoff);
     double operator()(float cutoff, float flatness, const Vector3d&xymin, const Vector3d&xymax, const Vector3d &uvmin, const Vector3d &uvmax){
+        //assert (cutoff*flatness>1);
         for (int i=0;i<MAX_CACHE;++i) {
-            if (cachedCutoff[i]==cutoff&&cachedFlatness[i]==flatness) {
+            if (cachedCutoff[i]==cutoff&&cachedFlatness[i]==flatness&&approxEqual(cachedDimensions[i][0],(xymax.x-xymin.x))&&approxEqual(cachedDimensions[i][1],(xymax.y-xymin.y))) {
                 return computeWithGivenCutoff(i,xymin,xymax,uvmin,uvmax);
             }
         }
@@ -24,8 +33,10 @@ public:
             if (infiniteIntegral[i]==0||i+1==MAX_CACHE) {
                 cachedCutoff[i]=cutoff;
                 cachedFlatness[i]=flatness;
+                cachedDimensions[i][0]=xymax.x-xymin.x;
+                cachedDimensions[i][1]=xymax.y-xymin.y;
                 infiniteIntegral[i]=1.0;
-                infiniteIntegral[i]=computeInfiniteIntegral(i);                
+                infiniteIntegral[i]=computeInfiniteIntegral(i);  
                 return computeWithGivenCutoff(i,xymin,xymax,uvmin,uvmax);
             }
         }
