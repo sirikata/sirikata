4f8623d75e8502af5587361bf9705ea22716b056
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2009-11-17 16:12:03 -0800

    Turn up number of main threads in object host to 2.

--- src/oh/main.cpp
+++ src/oh/main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char** argv) {
     ctx->add(ctx);
     ctx->add(obj_host);
     ctx->add(obj_factory);
-    ctx->run(1);
+    ctx->run(2);
 
     if (GetOption(PROFILE)->as<bool>()) {
         ctx->profiler->report();
