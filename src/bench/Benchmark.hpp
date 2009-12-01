06fc11b8cf7fdefd03435274c31ab1ba451a334f
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2009-12-01 15:52:51 -0800

    Add benchmark framework, binary, and a few timer benchmarks.

---
+++ src/bench/Benchmark.hpp
@@ -0,0 +1,85 @@
+/*  cbr
+ *  Benchmark.hpp
+ *
+ *  Copyright (c) 2009, Ewen Cheslack-Postava
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of cbr nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _CBR_BENCHMARK_HPP_
+#define _CBR_BENCHMARK_HPP_
+
+#include "Utility.hpp"
+#include "PollingService.hpp"
+
+namespace CBR {
+
+/** Benchmark is a service which performs a single micro-benchmark. Each
+ *  benchmark is a service -- it should start its test when start() is called
+ *  and should try its best to respect the call to stop().  If even that
+ *  fails, the benchmark will be forcibly destroyed.
+ *
+ *  The benchmark is passed a callback which it should invoke when the
+ *  benchmark has shutdown cleanly. This allows the benchmark to finish at
+ *  its own pace and clean up gracefully, unless the benchmark takes too
+ *  long
+ */
+class Benchmark : public Service {
+  public:
+    typedef std::tr1::function<void()> FinishedCallback;
+
+    /** Construct a benchmark which will invoke the specified callback when it
+     *  has completed.
+     */
+    Benchmark(const FinishedCallback& finished_cb)
+            : mFinishedCallback(finished_cb)
+    {}
+
+    virtual ~Benchmark() {}
+
+    /** Get the name of this benchmark, used for reporting. */
+    virtual String name() = 0;
+
+    virtual void start() = 0;
+    virtual void stop() = 0;
+
+  protected:
+    /** Notify the parent of this benchmark that the run has finished.  This
+     *  should be used by benchmark implementations to indicate when they have
+     *  finished and cleaned up without issues.
+     */
+    void notifyFinished() const {
+        mFinishedCallback();
+    }
+
+  private:
+    FinishedCallback mFinishedCallback;
+}; // class Benchmark
+
+} // namespace CBR
+
+#endif //_CBR_BENCHMARK_HPP_
---
+++ src/bench/main.cpp
@@ -0,0 +1,87 @@
+/*  cbr
+ *  main.cpp
+ *
+ *  Copyright (c) 2009, Ewen Cheslack-Postava
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of cbr nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "BenchmarkRunner.hpp"
+
+#include "TimerSpeedBenchmark.hpp"
+#include "TimerJitterBenchmark.hpp"
+#include "TimerMonotonicityBenchmark.hpp"
+
+using namespace CBR;
+
+typedef std::list<String> BenchmarkList;
+
+void runAll(BenchmarkRunner& runner, const BenchmarkList all_benchmarks) {
+    for(std::list<String>::const_iterator it = all_benchmarks.begin();
+        it != all_benchmarks.end();
+        it++)
+        runner.run(*it);
+}
+
+int main(int argc, char** argv) {
+    BenchmarkFactory factory;
+    BenchmarkList all_benchmarks;
+
+// Adds a benchmark to the factory and the list
+#define ADD_BENCHMARK(name, create_cb) \
+    factory.registerConstructor(#name, create_cb); \
+    all_benchmarks.push_back(#name)
+
+    // Benchmark registrations
+    ADD_BENCHMARK(timer-speed, TimerSpeedBenchmark::create);
+    ADD_BENCHMARK(timer-jitter, TimerJitterBenchmark::create);
+    ADD_BENCHMARK(timer-monotonicity, TimerMonotonicityBenchmark::create);
+
+
+    BenchmarkRunner runner(factory, Duration::seconds(30.f));
+
+
+    // No arguments, defaults to all benchmarks
+    if (argc < 2) {
+        runAll(runner, all_benchmarks);
+        return 0;
+    }
+
+    // Otherwise, run each one that's specified
+    for(int32 argsi = 1; argsi < argc; argsi++) {
+        String arg = argv[argsi];
+
+        if (arg == "all") {
+            runAll(runner, all_benchmarks);
+        }
+        else {
+            runner.run(arg);
+        }
+    }
+
+    return 0;
+}
