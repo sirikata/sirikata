776814f7c5ef5b54167adbe3cce29b8e8381fd69
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2010-04-06 15:54:55 -0700

    Use servers for craq that are actually alive.

--- scripts/cluster/craq/start_craq_server.py
+++ scripts/cluster/craq/start_craq_server.py
@@ -6,7 +6,7 @@ import time
 import socket
 
 def main():
-        if (not socket.gethostname() in ['meru28', 'meru29', 'meru30']):
+        if (not socket.gethostname() in ['meru27', 'meru29', 'meru30']):
             return 0
 
         subprocess.Popen('/home/meru/bmistree/new-craq-dist/craq-32 -d meru -p 10333 -z 192.168.1.7:9888', shell=True)
