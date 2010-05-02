a9cb57b944f1a4927e8c30501fc592f66acd96c5
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2010-05-02 16:59:28 -0700

    make sqr integral more efficient by estimating object center and using that instead of doing an integral--that works for objects smaller than YOUR MOM

--- src/SqrIntegral.hpp
+++ src/SqrIntegral.hpp
@@ -24,32 +24,13 @@ public:
     double computeWithGivenCutoff(int whichCutoff,const Vector3d&xymin, const Vector3d&xymax, const Vector3d &uvmin, const Vector3d &uvmax);
     double computeInfiniteIntegral(int whichCutoff);
     double operator()(float cutoff, float flatness, const Vector3d&xymin, const Vector3d&xymax, const Vector3d &uvmin, const Vector3d &uvmax){
-        if (!normalize) {
-            double error=0;
-            assert(xymin.x<=xymax.x);
-            assert(xymin.y<=xymax.y);
-            assert(uvmin.x<=uvmax.x);
-            assert(uvmin.y<=uvmax.y);
-            return integrate(cutoff,flatness,xymin,xymax,uvmin,uvmax,&error);
-        }
-        //assert (cutoff*flatness>1);
-        for (int i=0;i<MAX_CACHE;++i) {
-            if (cachedCutoff[i]==cutoff&&cachedFlatness[i]==flatness&&approxEqual(cachedDimensions[i][0],(xymax.x-xymin.x))&&approxEqual(cachedDimensions[i][1],(xymax.y-xymin.y))) {
-                return computeWithGivenCutoff(i,xymin,xymax,uvmin,uvmax);
-            }
-        }
-        for (int i=0;i<MAX_CACHE;++i) {
-            if (infiniteIntegral[i]==0||i+1==MAX_CACHE) {
-                cachedCutoff[i]=cutoff;
-                cachedFlatness[i]=flatness;
-                cachedDimensions[i][0]=xymax.x-xymin.x;
-                cachedDimensions[i][1]=xymax.y-xymin.y;
-                infiniteIntegral[i]=1.0;
-                infiniteIntegral[i]=computeInfiniteIntegral(i);  
-                return computeWithGivenCutoff(i,xymin,xymax,uvmin,uvmax);
-            }
-        }
-        return 0;
+        double deltaLocation=(((xymin+xymax)-(uvmin+uvmax))*.5).length();
+        deltaLocation+=cutoff;
+        if (deltaLocation<2.7) deltaLocation=2.7;
+        double logLocDelta=deltaLocation*log(deltaLocation);
+        double r1=(xymax-xymin).length();
+        double r2=(uvmax-uvmin).length();
+        return 2.*r1*r2/(logLocDelta*logLocDelta);
     }
 
 };
