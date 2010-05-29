2cf5a20f469348e3959a53677545a208bd1d1450
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2010-05-29 13:02:35 -0700

    make sure to reset origin before udpating in scripts

--- scripts/bench/runbench.sh
+++ scripts/bench/runbench.sh
@@ -9,7 +9,8 @@ git checkout -b master origin/master
 cd ..
 ./install-deps.sh update
 cd $HEREPLACE/../build/cmake&&cmake .&&make&&cd ../../scripts
-cluster/build.py update update_dependencies deploy Release
+cluster/build.py reset_origin update update_dependencies
+cluster/build.py deploy Release
 
 export OBJECTPROGRESSIONLIST="125 250 500 750 1000 1250 1500 1750 2000 2500 3000 3500 4000 4500 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000 17000 18000 19000"
 for i in $OBJECTPROGRESSIONLIST; do curl -O http://graphics.stanford.edu/~danielrh/4x4traces/messagetrace.$i; done
