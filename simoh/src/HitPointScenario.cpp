e634dbd2df29407ab839362fc3038e08a87d2228
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2010-09-02 18:10:47 -0700

    actually start poller and don't send out of date updates the first time

--- simoh/src/HitPointScenario.cpp
+++ simoh/src/HitPointScenario.cpp
@@ -123,7 +123,6 @@ public:
                 SILOG(hitpoint,error,"Got connection "<<mParent->object->uuid().toString()<<" - "<<mID.toString());
                 this->mSendStream = s;
                 s->registerReadCallback( std::tr1::bind( &DamagableObject::ReceiveDamage::senderShouldNotGetUpdate, this, _1, _2)  ) ;
-                sendUpdate();
             }
         }
         void login(int err, boost::shared_ptr< Stream<UUID> > s) {
@@ -136,11 +135,6 @@ public:
         }
         void sendUpdate() {
             if (mSendStream) {
-                static bool first=true;
-                if (first) {
-                    SILOG(hitpoint,error,"Warmed up");
-                    first=false;
-                }
                 int tosend=sizeof(DamagableObject::HPTYPE);
                 if (mPartialSendCount) {
                     memcpy(mPartialSend+sizeof(DamagableObject::HPTYPE),&mParent->mHP,sizeof(DamagableObject::HPTYPE));
@@ -149,7 +143,7 @@ public:
                 }else {
                     memcpy(mPartialSend,&mParent->mHP,tosend);
                 }
-                SILOG(hitpoint,error,"Sent buffer "<<makeHexString(mPartialSend+mPartialSendCount,tosend)<<" AT TIME ");
+                //SILOG(hitpoint,error,"Sent buffer "<<makeHexString(mPartialSend+mPartialSendCount,tosend)<<" AT TIME ");
                 mPartialSendCount+=mSendStream->write(mPartialSend+mPartialSendCount,tosend);
                 if(mPartialSendCount>=sizeof(DamagableObject::HPTYPE)) {
                     mPartialSendCount-=sizeof(DamagableObject::HPTYPE);
@@ -165,7 +159,7 @@ public:
             SILOG(hitpoint,error,"Should not receive updates from receiver");
         }
         void getUpdate(uint8*buffer, int length) {
-            SILOG(hitpoint,error,"Received buffer "<<makeHexString(buffer,length));
+            //SILOG(hitpoint,error,"Received buffer "<<makeHexString(buffer,length));
             while (length>0) {
                 
                 int datacopied = (length>sizeof(DamagableObject::HPTYPE)-mPartialCount?sizeof(DamagableObject::HPTYPE)-mPartialCount:length);
@@ -174,7 +168,8 @@ public:
                 if (mPartialCount==sizeof(DamagableObject::HPTYPE)) {
                     DamagableObject::HPTYPE hp;
                     memcpy(&hp,mPartialUpdate,mPartialCount);
-                    SILOG(hitpoint,error,"Got hitpoint update for "<<mID.toString()<<" as "<<hp<<" Delay "<<(hp-mContext->calcHp())<<" distance "<<(mObj->location().position()-mParent->object->location().position()).length());
+                    if (rand()%10==0)
+                        SILOG(hitpoint,error,"Got hitpoint update for "<<mID.toString()<<" as "<<hp<<" Delay "<<(hp-mContext->calcHp())<<" distance "<<(mObj->location().position()-mParent->object->location().position()).length());
                     
                     mPartialCount=0;
                 }
@@ -324,6 +319,7 @@ void HitPointScenario::delayedStart() {
         }
     }
     mGeneratePingPoller->start();
+    mHPPoller->start();
     mPingPoller->start();
 
 }
