586c1dd33f0a7d780081c8e02568da5342f06c0f
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-01-04 15:55:36 -0800

    Fix unsafe storage of pointer to mesh URI in DistanceDownloadPlanner.

--- libproxyobject/plugins/ogre/resourceManager/DistanceDownloadPlanner.cpp
+++ libproxyobject/plugins/ogre/resourceManager/DistanceDownloadPlanner.cpp
@@ -80,11 +80,10 @@ void DistanceDownloadPlanner::removeObject(ProxyObjectPtr p) {
 void DistanceDownloadPlanner::onSetMesh(ProxyObjectPtr proxy, URI const &meshFile)
 {
     vector<Resource>::iterator it = findResource(proxy);
-    it->file = &(URI&)meshFile;
+    it->file = meshFile;
     it->ready = true;
     proxy->priority = calculatePriority(proxy);
-    it->mesh->processMesh(meshFile);
-
+    it->mesh->processMesh(it->file);
 }
 
 double DistanceDownloadPlanner::calculatePriority(ProxyObjectPtr proxy)
