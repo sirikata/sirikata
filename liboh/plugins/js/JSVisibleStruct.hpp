bb45069fa76f7ea99e70709b4b5fe16a63ef8e84
bmistree <bmistree@stanford.edu>
2011-01-13 15:39:23 -0800

    Forgot to commit the visible structs.

---
+++ liboh/plugins/js/JSVisibleStruct.hpp
@@ -0,0 +1,59 @@
+
+
+
+#ifndef _SIRIKATA_JS_VISIBLE_STRUCT_HPP_
+#define _SIRIKATA_JS_VISIBLE_STRUCT_HPP_
+
+#include <sirikata/oh/HostedObject.hpp>
+#include <v8.h>
+#include <vector>
+
+namespace Sirikata {
+namespace JS {
+
+//need to forward-declare this so that can reference this inside
+class JSObjectScript;
+
+struct JSVisibleStruct
+{
+    JSVisibleStruct(JSObjectScript* parent, const SpaceObjectReference& whatsVisible, const SpaceObjectReference& toWhom, bool visibleCurrently, const Vector3d& currentPosition)
+     : jsObjScript(parent),
+       whatIsVisible(new SpaceObjectReference(whatsVisible)),
+       visibleToWhom(new SpaceObjectReference( toWhom)),
+       stillVisible(new bool(visibleCurrently)),
+       mPosition(new Vector3d(currentPosition))
+    {
+    }
+    
+    
+    ~JSVisibleStruct()
+    {
+        //do not delete jsObjScript: someone else is responsible for that.
+        delete whatIsVisible;
+        delete stillVisible;
+        delete mPosition;
+    }
+
+    //for decoding
+    static JSVisibleStruct* decodeVisible(v8::Handle<v8::Value> senderVal,std::string& errorMessage);
+
+    //methods mapped to javascript's visible object 
+    v8::Handle<v8::Value> returnProxyPosition();
+    v8::Handle<v8::Value> toString();
+    v8::Handle<v8::Value> printData();
+    v8::Handle<v8::Value> getStillVisible();
+    v8::Handle<v8::Value> visibleSendMessage (std::string& msgToSend);
+    
+    //data
+    JSObjectScript* jsObjScript;
+    SpaceObjectReference* whatIsVisible;
+    SpaceObjectReference* visibleToWhom;
+    bool* stillVisible;
+    Vector3d* mPosition;
+};
+
+
+}//end namespace js
+}//end namespace sirikata
+
+#endif
