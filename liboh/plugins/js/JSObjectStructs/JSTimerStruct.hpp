5db2920d4f0e69e5d70cdd5faac59f12772b4abc
bmistree <bmistree@stanford.edu>
2011-01-24 18:19:51 -0800

    Forgot to commit the timer struct classes.

---
+++ liboh/plugins/js/JSObjectStructs/JSTimerStruct.hpp
@@ -0,0 +1,43 @@
+
+#ifndef __SIRIKATA_JS_TIMER_STRUCT_HPP__
+#define __SIRIKATA_JS_TIMER_STRUCT_HPP__
+
+#include "../JSUtil.hpp"
+#include "../JSObjectScript.hpp"
+#include "JSContextStruct.hpp"
+#include <v8.h>
+#include <sirikata/core/network/IOTimer.hpp>
+#include <sirikata/core/network/IOService.hpp>
+
+
+
+namespace Sirikata {
+namespace JS {
+
+struct JSTimerStruct{
+    
+
+    JSTimerStruct(JSObjectScript* jsobj, const Duration& dur,v8::Persistent<v8::Object>& targ, v8::Persistent<v8::Function>& callback,JSContextStruct* jscont, Sirikata::Network::IOService* ioserve);
+    
+    ~JSTimerStruct();
+    
+    static JSTimerStruct* decodeTimerStruct(v8::Handle<v8::Value> toDecode,String errorMessage);
+    
+    v8::Handle<v8::Value> struct_clear();
+    v8::Handle<v8::Value> struct_resetTimer(double timeInSecondsToRefire);
+    void evaluateCallback();
+    
+    Sirikata::Network::DeadlineTimer* mDeadlineTimer;
+    JSObjectScript* jsObjScript;
+    v8::Persistent<v8::Object> target;
+    v8::Persistent<v8::Function> cb;
+
+    JSContextStruct* jsContStruct;
+
+
+};
+
+}  //end js namespace
+}  //end sirikata namespace
+
+#endif
