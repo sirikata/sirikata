81c7c90515de28c1e88cc9c1daad81d686600c4f
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-02-01 16:49:32 -0800

    Remove some dead code in Ogre's Entity class.

--- libproxyobject/plugins/ogre/Entity.cpp
+++ libproxyobject/plugins/ogre/Entity.cpp
@@ -1056,84 +1056,12 @@ void Entity::createMesh(MeshdataPtr mdptr) {
             mat->getTechnique(0)->getPass(0)->createTextureUnitState(ogreTextureName,0);
         }
         Ogre::MeshManager& mm = Ogre::MeshManager::getSingleton();
-        if (1) {
-            /// FIXME: set bounds, bounding radius here
-            Ogre::ManualResourceLoader *reload;
-            Ogre::MeshPtr mo (mm.createManual(hash,Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,(reload=OGRE_NEW MeshdataManualLoader(md))));
-            reload->prepareResource(&*mo);
-            reload->loadResource(&*mo);
-        }else {
-            /// FIXME: set bounds, bounding radius here
-            Ogre::ManualObject mo(hash);
-            mo.clear();
 
-            for(GeometryInstanceList::const_iterator geoinst_it = md.instances.begin(); geoinst_it != md.instances.end(); geoinst_it++) {
-                const GeometryInstance& geoinst = *geoinst_it;
-
-                Matrix4x4f pos_xform = md.getTransform(geoinst);
-                Matrix3x3f normal_xform = pos_xform.extract3x3().inverseTranspose();
-
-                // Get the instanced submesh
-                assert(geoinst.geometryIndex < md.geometry.size());
-                const SubMeshGeometry& submesh = md.geometry[geoinst.geometryIndex];
-
-                int vertcount = submesh.positions.size();
-                int normcount = submesh.normals.size();
-                for (size_t primitive_index = 0; primitive_index<submesh.primitives.size(); ++primitive_index) {
-                    const SubMeshGeometry::Primitive& prim=submesh.primitives[primitive_index];
-                    int indexcount = prim.indices.size();
-
-                    // FIXME select proper texture/material
-                    std::string matname = md.textures.size() > 0 ?
-                        hash + "_texture_" + md.textures[0] :
-                        base_mat->getName();
-                    mo.begin(matname);
-                    std::cerr<<"Begin "<<matname<<"\n";
-                    float tu, tv;
-                    for (int i=0; i<indexcount; i++) {
-                        int j = prim.indices[i];
-                        Vector3f v = submesh.positions[j];
-                        Vector4f v_xform = pos_xform * Vector4f(v[0], v[1], v[2], 1.f);
-                        v = Vector3f(v_xform[0], v_xform[1], v_xform[2]);
-                        mo.position(v[0], v[1], v[2]);
-                        std::cerr<<"Mo pos "<<v[0]<<","<<v[1]<<","<<v[2]<<"\n";
-                        Vector3f normal = submesh.normals[j];
-                        normal = normal_xform * normal;
-                        mo.normal(normal[0], normal[1], normal[2]);
-                        std::cerr<<"Mo norm "<<normal[0]<<","<<normal[1]<<","<<normal[2]<<"\n";
-                        mo.colour(1.0,1.0,1.0,1.0);
-                        if (submesh.texUVs.size()==0) {
-                            /// bogus texture for textureless models
-                            if (i%3==0) {
-                                tu=0.0;
-                                tv=0.0;
-                            }
-                            if (i%3==1) {
-                                tu=0.0;
-                                tv=1.0;
-                            }
-                            if (i%3==2) {
-                                tu=1.0;
-                                tv=1.0;
-                            }
-                        }
-                        else {
-                            Sirikata::Vector2f uv (submesh.texUVs[0].uvs[ j*submesh.texUVs[0].stride ],
-                                                   submesh.texUVs[0].uvs[ j*submesh.texUVs[0].stride+1]);
-                            tu = uv[0];
-                            tv = 1.0-uv[1];           //  why you gotta be like that?
-                        }
-                        std::cerr<<"Mo tex "<<tu<<","<<tv<<"\n";
-                        mo.textureCoord(tu, tv);
-                    }
-                    std::cerr<<"Mo end\n";
-                    mo.end();
-                }
-            } // submesh
-
-            mo.setVisible(true);
-            Ogre::MeshPtr mp = mo.convertToMesh(hash, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-        }
+        /// FIXME: set bounds, bounding radius here
+        Ogre::ManualResourceLoader *reload;
+        Ogre::MeshPtr mo (mm.createManual(hash,Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,(reload=OGRE_NEW MeshdataManualLoader(md))));
+        reload->prepareResource(&*mo);
+        reload->loadResource(&*mo);
 
         bool check = mm.resourceExists(hash);
 
