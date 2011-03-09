df317a4f41239c5a78e69c78208268a47e52912b
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-03-09 10:32:03 -0800

    Fix sgrep to handle relative paths, also exclude documentation directories.

--- sgrep.py
+++ sgrep.py
@@ -3,11 +3,14 @@
 import subprocess
 import sys
 
-EXCLUDES = [ 'dependencies', 'externals', 'scripts', 'build/cmake', 'build/Frameworks', '.git' ]
+EXCLUDES = [ 'dependencies', 'externals', 'scripts', 'build/cmake', 'build/Frameworks', '.git', 'doc', 'docs' ]
 
 if __name__ == "__main__":
     if (len (sys.argv) < 2):
         print("\n\nIncorrect usage: add in what you're grepping for\n\n")
+
+    relative_excludes = [ './' + ex for ex in EXCLUDES ]
+    EXCLUDES.extend(relative_excludes)
 
     #cmd = ['grep','-R', sys.argv[1]]
     cmd = ['grep','-R']
