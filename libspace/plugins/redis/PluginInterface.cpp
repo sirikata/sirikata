e6f3fa9c1b885aa00bdafa530402785d9645b766
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-03-21 18:55:45 -0700

    Add the base of a Redis OSeg using the Local OSeg as a skeleton.

---
+++ libspace/plugins/redis/PluginInterface.cpp
@@ -0,0 +1,93 @@
+/*  Sirikata
+ *  PluginInterface.cpp
+ *
+ *  Copyright (c) 2011, Ewen Cheslack-Postava
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of Sirikata nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sirikata/core/util/Platform.hpp>
+#include <sirikata/core/options/Options.hpp>
+#include <sirikata/space/ObjectSegmentation.hpp>
+#include "RedisObjectSegmentation.hpp"
+
+static int space_redis_plugin_refcount = 0;
+
+namespace Sirikata {
+
+static void InitPluginOptions() {
+    Sirikata::InitializeClassOptions ico("space_redis", NULL,
+        NULL);
+}
+
+static ObjectSegmentation* createRedisOSeg(SpaceContext* ctx, Network::IOStrand* oseg_strand, CoordinateSegmentation* cseg, OSegCache* cache, const String& args) {
+    OptionSet* optionsSet = OptionSet::getOptions("space_redis",NULL);
+    optionsSet->parse(args);
+
+    return new RedisObjectSegmentation(ctx, oseg_strand, cseg, cache);
+}
+
+} // namespace Sirikata
+
+SIRIKATA_PLUGIN_EXPORT_C void init() {
+    using namespace Sirikata;
+    if (space_redis_plugin_refcount==0) {
+        InitPluginOptions();
+        using std::tr1::placeholders::_1;
+        using std::tr1::placeholders::_2;
+        using std::tr1::placeholders::_3;
+        using std::tr1::placeholders::_4;
+        using std::tr1::placeholders::_5;
+        OSegFactory::getSingleton()
+            .registerConstructor("redis",
+                std::tr1::bind(&createRedisOSeg, _1, _2, _3, _4, _5));
+    }
+    space_redis_plugin_refcount++;
+}
+
+SIRIKATA_PLUGIN_EXPORT_C int increfcount() {
+    return ++space_redis_plugin_refcount;
+}
+SIRIKATA_PLUGIN_EXPORT_C int decrefcount() {
+    assert(space_redis_plugin_refcount>0);
+    return --space_redis_plugin_refcount;
+}
+
+SIRIKATA_PLUGIN_EXPORT_C void destroy() {
+    using namespace Sirikata;
+    if (space_redis_plugin_refcount==0) {
+        OSegFactory::getSingleton().unregisterConstructor("redis");
+    }
+}
+
+SIRIKATA_PLUGIN_EXPORT_C const char* name() {
+    return "redis";
+}
+
+SIRIKATA_PLUGIN_EXPORT_C int refcount() {
+    return space_redis_plugin_refcount;
+}
---
+++ libspace/plugins/redis/RedisObjectSegmentation.cpp
@@ -0,0 +1,84 @@
+/*  Sirikata
+ *  RedisObjectSegmentation.cpp
+ *
+ *  Copyright (c) 2010, Ewen Cheslack-Postava
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of Sirikata nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "RedisObjectSegmentation.hpp"
+
+#define REDISOSEG_LOG(lvl,msg) SILOG(redis_oseg, lvl, "[REDISOSEG] " << msg)
+
+namespace Sirikata {
+
+RedisObjectSegmentation::RedisObjectSegmentation(SpaceContext* con, Network::IOStrand* o_strand, CoordinateSegmentation* cseg, OSegCache* cache)
+ : ObjectSegmentation(con, o_strand),
+   mCSeg(cseg),
+   mCache(cache)
+{
+}
+
+OSegEntry RedisObjectSegmentation::cacheLookup(const UUID& obj_id) {
+    // We only check the cache for statistics purposes
+    return mCache->get(obj_id);
+}
+
+OSegEntry RedisObjectSegmentation::lookup(const UUID& obj_id) {
+    // Redis, so all objects should be available.
+    OSegMap::const_iterator it = mOSeg.find(obj_id);
+    if (it == mOSeg.end()) {
+        REDISOSEG_LOG(warn, "Couldn't find object OSegEntry in RedisObjectSegmentation.");
+        return OSegEntry::null();
+    }
+
+    return it->second;
+}
+
+void RedisObjectSegmentation::addNewObject(const UUID& obj_id, float radius) {
+    mOSeg[obj_id] = OSegEntry(mContext->id(), radius);
+    mWriteListener->osegWriteFinished(obj_id);
+}
+
+void RedisObjectSegmentation::addMigratedObject(const UUID& obj_id, float radius, ServerID idServerAckTo, bool) {
+    mOSeg[obj_id] = OSegEntry(mContext->id(), radius);
+}
+
+void RedisObjectSegmentation::removeObject(const UUID& obj_id) {
+    mOSeg.erase(obj_id);
+}
+
+bool RedisObjectSegmentation::clearToMigrate(const UUID& obj_id) {
+    REDISOSEG_LOG(error, "RedisObjectSegmentation got clearToMigrate call which should be impossible with single server.");
+    return false;
+}
+
+void RedisObjectSegmentation::migrateObject(const UUID& obj_id, const OSegEntry& new_server_id) {
+    REDISOSEG_LOG(error, "RedisObjectSegmentation got migrateObject call which should be impossible with single server.");
+}
+
+} // namespace Sirikata
