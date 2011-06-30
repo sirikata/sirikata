748564bdb9239d10d052a23985b28d9ce44ffa0c
bmistree <bmistree@stanford.edu>
2011-06-30 15:16:34 -0700

    Blacklisted a few more folders in sgrep (mostly in web gui stuff for ogre).

--- sgrep.py
+++ sgrep.py
@@ -3,7 +3,7 @@
 import subprocess
 import sys
 
-EXCLUDES = [ 'dependencies', 'externals', 'scripts', 'build/cmake', 'build/Frameworks', '.git', 'doc', 'docs', 'libproxyobject/plugins/ogre/data/ace' ]
+EXCLUDES = [ 'dependencies', 'externals', 'scripts', 'build/cmake', 'build/Frameworks', '.git', 'doc', 'docs', 'libproxyobject/plugins/ogre/data/ace', 'libproxyobject/plugins/ogre/data/labjs' , 'libproxyobject/plugins/ogre/data/jquery_themes', 'libproxyobject/plugins/ogre/data/jquery_plugins' , 'libproxyobject/plugins/ogre/data/jquery' ]
 
 if __name__ == "__main__":
     if (len (sys.argv) < 2):
