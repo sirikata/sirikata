66faae8bf42d80c4766081f8d1647b7fbb1f102b
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-07-22 13:36:23 -0700

    Ensure each HostedObject only gets one stop() call, even if it has multiple presences.

--- liboh/src/ObjectHost.cpp
+++ liboh/src/ObjectHost.cpp
@@ -298,10 +298,18 @@ void ObjectHost::stop() {
         sm->stop();
     }
 
+    // HostedObjects may appear multiple times because they may have
+    // multiple presences. Track which we've called stop on so we
+    // don't double-stop any of them.
+    std::tr1::unordered_set<UUID, UUID::Hasher> seen_hosted_objects;
     for (HostedObjectMap::iterator iter = mHostedObjects.begin();
          iter != mHostedObjects.end();
          ++iter) {
-        iter->second->stop();
+        HostedObjectPtr ho = iter->second;
+        if (seen_hosted_objects.find(ho->id()) == seen_hosted_objects.end()) {
+            seen_hosted_objects.insert(ho->id());
+            ho->stop();
+        }
     }
 }
 
