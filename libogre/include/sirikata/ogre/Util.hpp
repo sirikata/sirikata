6664a1ae758cac2aea9b3cd6e4cfd20eecf02d3a
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-08-11 16:20:07 -0700

    Try loading non-image textures in WebViews.
    Detect http texture URLs and, when their data can't be parsed as an
    image, try loading them in a WebView and applying them as a texture.
    This allows you to use web pages as textures, and especially works
    nicely with billboards. Fixes #409.

---
+++ libogre/include/sirikata/ogre/Util.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2011 Sirikata Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
+#ifndef _SIRIKATA_OGRE_UTIL_HPP_
+#define _SIRIKATA_OGRE_UTIL_HPP_
+
+#include <sirikata/core/util/Logging.hpp>
+
+#define OGRE_LOG(lvl, msg) SILOG(ogre, lvl, msg)
+
+
+#endif //_SIRIKATA_OGRE_UTIL_HPP_
