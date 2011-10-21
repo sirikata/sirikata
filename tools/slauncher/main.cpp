f78f8cc63337585f578794c452fcfcd0c604e335
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-10-21 16:31:23 -0700

    Add slauncher binary for handling URIs and launching Sirikata binaries.
    Add slauncher binary to build. It can register (and unregister) a URI
    handler and launches other binaries based on URIs. The URI is minimal
    -- it just contains sirikata: as the scheme, followed by another URI
    that specifies a JSON configuration file. The configuration file
    specifies some properties, and in the future should be able to point
    to additional resources.

---
+++ tools/slauncher/main.cpp
@@ -0,0 +1,294 @@
+// Copyright (c) 2011 Sirikata Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
+
+#include <sirikata/core/service/Context.hpp>
+#include <sirikata/core/network/IOServiceFactory.hpp>
+#include <sirikata/core/network/IOWork.hpp>
+#include <sirikata/core/trace/Trace.hpp>
+#include <sirikata/core/util/Timer.hpp>
+#include <sirikata/core/options/Options.hpp>
+#include <sirikata/core/options/CommonOptions.hpp>
+#include <sirikata/core/transfer/URI.hpp>
+#include <sirikata/core/util/Paths.hpp>
+#include <boost/filesystem.hpp>
+#include <sirikata/core/transfer/AggregatedTransferPool.hpp>
+#include <sirikata/core/transfer/ResourceDownloadTask.hpp>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+
+#define LAUNCHER_LOG(lvl, msg) SILOG(launcher, lvl, msg)
+
+using namespace Sirikata;
+
+// ### Utils ###
+
+void InitLauncherOptions() {
+    InitializeClassOptions::module(SIRIKATA_OPTIONS_MODULE)
+        .addOption(new OptionValue("register","false",Sirikata::OptionValueType<bool>(),"If true, registers this binary as handler"))
+        .addOption(new OptionValue("unregister","false",Sirikata::OptionValueType<bool>(),"If true, unregisters this binary as handler"))
+        .addOption(new OptionValue("uri","",Sirikata::OptionValueType<String>(),"The URI to launch"))
+        ;
+}
+
+
+String getExecutablePath(String name) {
+    String exe_dir = Path::Get(Path::DIR_EXE);
+
+#if SIRIKATA_DEBUG
+    name = name + "_d";
+#endif
+#if SIRIKATA_PLATFORM == PLATFORM_WINDOWS
+    name = name + ".exe";
+#endif
+
+    return (boost::filesystem::path(exe_dir) / name).string();
+}
+
+
+#if SIRIKATA_PLATFORM == PLATFORM_LINUX
+void execCommand(const char* file, const char* const argv[]) {
+    pid_t pID = fork();
+
+    if (pID == 0) {
+        execvp(file, (char* const*)argv);
+    }
+    else if (pID < 0) {
+        LAUNCHER_LOG(fatal, "Couldn't fork to execute registration commands.");
+    }
+}
+#endif
+
+
+// ### Registration ###
+
+bool registerURIHandler() {
+    // Note that we actually ask for slauncher's path instead of just using this
+    // binary's. This ensures we use the generic (rather than versioned)
+    // copy/symlink of the executable. This is just useful to handle upgrades
+    // gracefully, i.e. without having to reregister.
+    String exe_file = getExecutablePath("slauncher");
+
+#if SIRIKATA_PLATFORM == PLATFORM_LINUX
+    String cmd = exe_file + " --uri=%s";
+    String needs_terminal =
+#if SIRIKATA_DEBUG
+        "true"
+#else
+        "false"
+#endif
+        ;
+    const char* const command_argv[] = { "gconftool-2", "-t", "string", "-s", "/desktop/gnome/url-handlers/sirikata/command", cmd.c_str(), NULL};
+    execCommand("gconftool-2", command_argv);
+
+    const char* const needs_terminal_argv[] = { "gconftool-2", "-t", "bool", "-s", "/desktop/gnome/url-handlers/sirikata/needs_terminal", needs_terminal.c_str(), NULL};
+    execCommand("gconftool-2", needs_terminal_argv);
+
+    const char* const enabled_argv[] = { "gconftool-2", "-t", "bool", "-s", "/desktop/gnome/url-handlers/sirikata/enabled", "true", NULL};
+    execCommand("gconftool-2", enabled_argv);
+
+#elif SIRIKATA_PLATFORM == PLATFORM_WINDOWS
+    // FIXME
+    LAUNCHER_LOG(error, "URI handler registration not supported on this platform.");
+    return false;
+#elif SIRIKATA_PLATFORM == PLATFORM_MAC
+    // FIXME
+    LAUNCHER_LOG(error, "URI handler registration not supported on this platform.");
+    return false;
+#endif
+
+    LAUNCHER_LOG(info, "Registered launcher. You may have to restart your browser for this to take effect.");
+
+    return true;
+}
+
+bool unregisterURIHandler() {
+    // See note in registerURIHandler
+    String exe_file = getExecutablePath("slauncher");
+
+#if SIRIKATA_PLATFORM == PLATFORM_LINUX
+    const char* const enabled_argv[] = { "gconftool-2", "-t", "bool", "-s", "/desktop/gnome/url-handlers/sirikata/enabled", "false", NULL};
+    execCommand("gconftool-2", enabled_argv);
+#elif SIRIKATA_PLATFORM == PLATFORM_WINDOWS
+    // FIXME
+#elif SIRIKATA_PLATFORM == PLATFORM_MAC
+    // FIXME
+#endif
+
+    return false;
+}
+
+
+// ### Downloading config and launching code ###
+
+// Work to keep the system busy when other stuff, e.g. downloads, are going on
+// away from the main IOService
+Network::IOWork* ioWork = NULL;
+void startWork(Network::IOService* ios) {
+    ioWork = new Network::IOWork(*ios, "slauncher");
+}
+void stopWork() {
+    delete ioWork;
+    ioWork = NULL;
+}
+
+
+Transfer::ResourceDownloadTaskPtr rdl;
+void finishLaunchURI(Transfer::ChunkRequestPtr req, Transfer::DenseDataPtr data, int* retval);
+bool startLaunchURI(String uri_str, Transfer::TransferPoolPtr transferPool, int* retval) {
+    Transfer::URI uri(uri_str);
+    if (uri.scheme() != "sirikata") {
+        LAUNCHER_LOG(error, "slauncher only supports sirikata URIs");
+        return false;
+    }
+
+    // Format for the URI: sirikata:real_config_url
+    String config_uri_str = uri.schemeSpecificPart();
+    // We assume the second part is a URI that we support
+    Transfer::URI config_uri(config_uri_str);
+    if (config_uri.empty()) {
+        LAUNCHER_LOG(error, "Invalid config URL in slauncher URI.");
+        return false;
+    }
+
+    LAUNCHER_LOG(detailed, "Loading from " << config_uri_str);
+
+    rdl =
+        Transfer::ResourceDownloadTask::construct(
+            config_uri, transferPool, 1.0,
+            std::tr1::bind(finishLaunchURI, std::tr1::placeholders::_1, std::tr1::placeholders::_2, retval)
+        );
+    rdl->start();
+
+    return true;
+}
+
+void eventLoopExit(int* retval, int val) {
+    stopWork();
+    *retval = val;
+}
+
+void finishLaunchURI(Transfer::ChunkRequestPtr req, Transfer::DenseDataPtr data, int* retval) {
+    // Fire off the request for
+    if (!data || data->size() == 0) {
+        LAUNCHER_LOG(error, "Failed to download config");
+        eventLoopExit(retval, -1);
+        return;
+    }
+
+    LAUNCHER_LOG(detailed, "Downloaded config, size " << data->size());
+
+    // Parse the data
+    using namespace boost::property_tree;
+    ptree pt;
+    try {
+        std::stringstream data_json(data->asString());
+        read_json(data_json, pt);
+    }
+    catch(json_parser::json_parser_error exc) {
+        LAUNCHER_LOG(error, "Invalid JSON configuration data.");
+        eventLoopExit(retval, -1);
+        return;
+    }
+
+
+    String app;
+    String appDir;
+    String binary;
+    std::vector<String> binaryArgs;
+    try {
+        app = pt.get<String>("app.name");
+        appDir = pt.get<String>("app.directory");
+        // First, we need to know what app we're executing
+        binary = pt.get<String>("binary.name");
+        // We also take a dictionary of arguments to add to the command
+        // line
+        BOOST_FOREACH(ptree::value_type &v,
+            pt.get_child("binary.args"))
+            binaryArgs.push_back( String("--") + v.first + "=" + v.second.data() );
+    }
+    catch(ptree_bad_data exc) {
+        LAUNCHER_LOG(error, "Required configuration properties not found.");
+        eventLoopExit(retval, -1);
+        return;
+    }
+
+    // We want to add support for syncing additional content (maybe
+    // something like app.data = url for archive) which will require
+    // additional downloading, but for now we just invoke the command
+    // here.
+#if SIRIKATA_PLATFORM == PLATFORM_LINUX
+    String appExe = getExecutablePath(binary);
+    binaryArgs.insert(binaryArgs.begin(), appExe);
+    const char** execArgs = new const char*[binaryArgs.size()+1];
+    for(uint32 i = 0; i < binaryArgs.size(); i++)
+        execArgs[i] = binaryArgs[i].c_str();
+    execArgs[binaryArgs.size()] = NULL;
+    execCommand(appExe.c_str(), execArgs);
+#elif SIRIKATA_PLATFORM == PLATFORM_MAC
+    // FIXME
+#elif SIRIKATA_PLATFORM == PLATFORM_WINDOWS
+    // FIXME
+#endif
+    eventLoopExit(retval, 0);//*retval = 0;
+}
+
+// ### Driver ###
+
+int main(int argc, char** argv) {
+
+    // Init and parse options
+
+    InitOptions();
+    InitLauncherOptions();
+    ParseOptions(argc, argv);
+
+    // Special simple case for registering handlers
+
+    bool registerHandler = GetOptionValue<bool>("register");
+    if (registerHandler)
+        return (registerURIHandler() ? 0 : -1);
+
+    bool unregisterHandler = GetOptionValue<bool>("unregister");
+    if (unregisterHandler)
+        return (unregisterURIHandler() ? 0 : -1);
+
+    // Basic app setup
+
+    Network::IOService* ios = Network::IOServiceFactory::makeIOService();
+    Network::IOStrand* iostrand = ios->createStrand();
+    startWork(ios);
+
+    Trace::Trace* trace = new Trace::Trace("slauncher.log");
+    Time epoch = Timer::now();
+
+    Context* ctx = new Context("slauncher", ios, iostrand, trace, epoch);
+
+    Transfer::TransferPoolPtr transferPool = Transfer::TransferMediator::getSingleton().registerClient<Transfer::AggregatedTransferPool>("slauncher");
+
+    // Start loading
+
+    String uri = GetOptionValue<String>("uri");
+    int retval = 0;
+    bool launched = startLaunchURI(uri, transferPool, &retval);
+    // Only start the process if we
+    if (!launched) return -1;
+
+    // Run
+    ctx->add(ctx);
+    ctx->run(1);
+
+    // Cleanup
+    ctx->cleanup();
+    trace->prepareShutdown();
+    delete ctx;
+    delete trace;
+
+    delete iostrand;
+    Network::IOServiceFactory::destroyIOService(ios);
+
+    return retval;
+}
