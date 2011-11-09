1795e6ed757212091b6dce1541f5e38f6142e7d5
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-11-09 14:01:58 -0800

    Track object query and session info and map to server query init/destruction in ManualObjectQueryProcessor.

--- liboh/plugins/manual_query/ManualObjectQueryProcessor.cpp
+++ liboh/plugins/manual_query/ManualObjectQueryProcessor.cpp
@@ -42,11 +42,78 @@ void ManualObjectQueryProcessor::onSpaceNodeSession(const OHDP::SpaceNodeID& id,
 void ManualObjectQueryProcessor::onSpaceNodeSessionEnded(const OHDP::SpaceNodeID& id) {
 }
 
-void ManualObjectQueryProcessor::onObjectNodeSession(const SpaceID& space, const ObjectReference& sporef, const OHDP::NodeID& id) {
-    QPLOG(detailed, "New object-space node session: " << sporef << " connected to " << space << "-" << id);
+void ManualObjectQueryProcessor::onObjectNodeSession(const SpaceID& space, const ObjectReference& oref, const OHDP::NodeID& id) {
+    QPLOG(detailed, "New object-space node session: " << oref << " connected to " << space << "-" << id);
+
+    OHDP::SpaceNodeID snid(space, id);
+    SpaceObjectReference sporef(space, oref);
+
+    ObjectStateMap::iterator obj_it = mObjectState.find(sporef);
+
+    // First, clear out old state
+    if (obj_it != mObjectState.end() && obj_it->second.node != OHDP::NodeID::null()) {
+        OHDP::SpaceNodeID old_snid(obj_it->first.space(), obj_it->second.node);
+        ServerQueryMap::iterator old_serv_it = mServerQueries.find(old_snid);
+        assert(old_serv_it != mServerQueries.end());
+        decrementServerQuery(old_serv_it);
+    }
+
+    // Update state
+    // Object no longer has a session, it's no longer relevant
+    if (id == OHDP::NodeID::null()) {
+        mObjectState.erase(obj_it);
+        return;
+    }
+    // Otherwise it's new/migrating
+    if (obj_it == mObjectState.end())
+        obj_it = mObjectState.insert( ObjectStateMap::value_type(sporef, ObjectState()) ).first;
+    obj_it->second.node = id;
+    // Figure out if we need to do anything on the
+    ServerQueryMap::iterator serv_it = mServerQueries.find(snid);
+    if (serv_it == mServerQueries.end())
+        serv_it = mServerQueries.insert( ServerQueryMap::value_type(snid, ServerQueryState()) ).first;
+    incrementServerQuery(serv_it);
+}
+
+void ManualObjectQueryProcessor::incrementServerQuery(ServerQueryMap::iterator serv_it) {
+    bool is_new = (serv_it->second.nconnected == 0);
+    serv_it->second.nconnected++;
+    updateServerQuery(serv_it, is_new);
+}
+
+void ManualObjectQueryProcessor::updateServerQuery(ServerQueryMap::iterator serv_it, bool is_new) {
+    if (is_new) {
+        QPLOG(detailed, "Initializing server query to " << serv_it->first);
+        // FIXME send init message
+    }
+}
+
+void ManualObjectQueryProcessor::decrementServerQuery(ServerQueryMap::iterator serv_it) {
+    serv_it->second.nconnected--;
+    if (!serv_it->second.canRemove()) return;
+
+    // FIXME send message
+    QPLOG(detailed, "Destroying server query to " << serv_it->first);
+
+    mServerQueries.erase(serv_it);
 }
 
 void ManualObjectQueryProcessor::updateQuery(HostedObjectPtr ho, const SpaceObjectReference& sporef, const String& new_query) {
+    ObjectStateMap::iterator it = mObjectState.find(sporef);
+
+    if (new_query.empty()) { // Cancellation
+        // Remove from server query
+        // FIXME
+        // Clear object state if possible
+        if (it->second.canRemove())
+            mObjectState.erase(it);
+    }
+    else { // Init or update
+        // Track state
+        it->second.query = new_query;
+        // Update server query
+        // FIXME
+    }
 }
 
 } // namespace Manual
--- liboh/plugins/manual_query/ManualObjectQueryProcessor.hpp
+++ liboh/plugins/manual_query/ManualObjectQueryProcessor.hpp
@@ -37,13 +37,59 @@ public:
     virtual void onSpaceNodeSessionEnded(const OHDP::SpaceNodeID& id);
 
     // ObjectNodeSessionListener Interface
-    virtual void onObjectNodeSession(const SpaceID& space, const ObjectReference& sporef, const OHDP::NodeID& id);
+    virtual void onObjectNodeSession(const SpaceID& space, const ObjectReference& oref, const OHDP::NodeID& id);
 
     virtual void updateQuery(HostedObjectPtr ho, const SpaceObjectReference& sporef, const String& new_query);
 
 private:
 
     ObjectHostContext* mContext;
+
+    // Queries registered by objects to be resolved locally and
+    // connection state
+    struct ObjectState {
+        ObjectState()
+         : node(OHDP::NodeID::null()),
+           query()
+        {}
+
+        // Checks if it is safe to destroy this ObjectState
+        bool canRemove() const {
+            return (node == OHDP::NodeID::null());
+        }
+
+        OHDP::NodeID node;
+        String query;
+    };
+    typedef std::tr1::unordered_map<SpaceObjectReference, ObjectState, SpaceObjectReference::Hasher> ObjectStateMap;
+    ObjectStateMap mObjectState;
+
+    // Queries we've registered with servers so that we can resolve
+    // object queries
+    struct ServerQueryState {
+        ServerQueryState()
+         : nconnected(0)
+        {}
+
+        bool canRemove() const {
+            return nconnected == 0;
+        }
+
+        // # of connected objects
+        int32 nconnected;
+    };
+    typedef std::tr1::unordered_map<OHDP::SpaceNodeID, ServerQueryState, OHDP::SpaceNodeID::Hasher> ServerQueryMap;
+    ServerQueryMap mServerQueries;
+
+    // Helper that marks a
+    void incrementServerQuery(ServerQueryMap::iterator serv_it);
+    // Helper that updates a server query, possibly sending an
+    // initialization message to the server to setup the query
+    void updateServerQuery(ServerQueryMap::iterator serv_it, bool is_new);
+    // Helper that tries to remove the server query pointed at by the
+    // iterator, checking if it is referenced at all yet and sending a
+    // message to kill the query
+    void decrementServerQuery(ServerQueryMap::iterator serv_it);
 };
 
 } // namespace Manual
