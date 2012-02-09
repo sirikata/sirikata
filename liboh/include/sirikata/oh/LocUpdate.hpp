92611ab91bd6e95b24b12468defd5781f830a1d9
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-02-09 14:42:40 -0800

    Fix timestamps for orphan loc updates.
    The LocUpdate class was always adjusting timestamps using the server
    time offset. However, "orphan" loc updates that are generated from a
    removed ProxyObject's properties are already in local time.
    
    This fixes that by making the method that gets the location in local
    time virtual and overrides it for the class that wraps
    PresenceProperties to provide the LocUpdate interface.

--- liboh/include/sirikata/oh/LocUpdate.hpp
+++ liboh/include/sirikata/oh/LocUpdate.hpp
@@ -44,9 +44,10 @@ public:
      *  \param oh the ObjectHost to use to convert the times
      *  \param from_space the space to convert the time from
      */
-    TimedMotionVector3f locationWithLocalTime(ObjectHost* oh, const SpaceID& from_space) const;
+    virtual TimedMotionVector3f locationWithLocalTime(ObjectHost* oh, const SpaceID& from_space) const;
     /** Get the location in the update but convert it so the time is in local
-     *  time, using the given space as the source.
+     *  time, using the given space as the source. These are wrappers around the
+     *  version that takes an ObjectHost.
      *  \param ho the HostedObject to use to convert the times
      *  \param from_space the space to convert the time from
      */
@@ -62,9 +63,10 @@ public:
      *  \param oh the ObjectHost to use to convert the times
      *  \param from_space the space to convert the time from
      */
-    TimedMotionQuaternion orientationWithLocalTime(ObjectHost* oh, const SpaceID& from_space) const;
+    virtual TimedMotionQuaternion orientationWithLocalTime(ObjectHost* oh, const SpaceID& from_space) const;
     /** Get the orientation in the update but convert it so the time is in local
-     *  time, using the given space as the source.
+     *  time, using the given space as the source. These are wrappers around the
+     *  versions that take an ObjectHost.
      *  \param ho the HostedObject to use to convert the times
      *  \param from_space the space to convert the time from
      */
