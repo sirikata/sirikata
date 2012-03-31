59cdabfa963752ae42be5d30188b433a8335cec4
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-03-31 11:19:36 -0700

    Switch to latest version of cxxtest. Remove perl support as it's been removed from upstream cxxtest.

--- build/modules/CXXTestUtils.cmake
+++ build/modules/CXXTestUtils.cmake
@@ -14,16 +14,7 @@ MACRO(ADD_CXXTEST_CPP_TARGET)
 
   IF(PYTHON_EXECUTABLE)
     SET(CXXTEST_COMPILER ${PYTHON_EXECUTABLE})
-    SET(CXXTEST_GEN ${CXXTEST_LIBRARYDIR}/cxxtestgen.py)
-  ELSE()
-    FIND_PACKAGE(Perl)
-    IF(PERL_EXECUTABLE)
-      SET(CXXTEST_CPP_FILE test.cc)      #perl cannot output to a full path.
-      SET(CXXTEST_COMPILER ${PERL_EXECUTABLE})
-      SET(CXXTEST_GEN ${CXXTEST_LIBRARYDIR}/cxxtestgen.pl)
-    ELSE()
-      MESSAGE(STATUS "!!! Cannot locate python or perl -- tests will not be compiled.")
-    ENDIF()
+    SET(CXXTEST_GEN ${CXXTEST_LIBRARYDIR}/bin/cxxtestgen)
   ENDIF()
   FOREACH(FILE ${CXXTEST_FILES})
     SET(CXXTEST_H_FILE ${FILE})
@@ -33,12 +24,12 @@ MACRO(ADD_CXXTEST_CPP_TARGET)
     SET(FINAL_CXXTEST_COMMAND ${CXXTEST_COMPILER} ${CXXTEST_GEN} ${CXXTEST_OPTIONS} -o ${CXXTEST_CPP_FILE} ${CXXTEST_TEST_FILES})
     ADD_CUSTOM_COMMAND(OUTPUT ${CXXTEST_CPP_FILE}
                        COMMAND ${CXXTEST_COMPILER} ${CXXTEST_GEN} ${CXXTEST_OPTIONS} -o ${CXXTEST_CPP_FILE} ${CXXTEST_TEST_FILES}
-                       DEPENDS ${CXXTEST_TEST_FILES} ${CXXTEST_DEPENDS}
+                       DEPENDS ${CXXTEST_TEST_FILES} ${CXXTEST_DEPENDS} ${CXXTEST_GEN}
                        COMMENT "Building ${CXXTEST_TEST_FILES} -> ${CXXTEST_CPP_FILE}")
   ELSE()
     ADD_CUSTOM_COMMAND(OUTPUT ${CXXTEST_CPP_FILE}
                        COMMAND exit 1
-                       COMMENT "Unable to build ${CXXTEST_CPP_FILE} because python and perl were not found.")
+                       COMMENT "Unable to build ${CXXTEST_CPP_FILE} because python was not found.")
   ENDIF()
 
   SET_DIRECTORY_PROPERTIES(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${CXXTEST_CPP_FILE})
