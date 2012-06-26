7769c1d6e434961230c74806727dd7b7ee2afbf0
Jeff Terrace <jterrace@gmail.com>
2012-06-26 11:42:43 -0400

    Fixed some path issues with emerson documentation generator and added optional --sphinx parameter to generate sphinx documentation instead of jsdoc html

--- tools/doc/emersonDocs.py
+++ tools/doc/emersonDocs.py
@@ -4,15 +4,22 @@ import sys
 import subprocess
 import os.path
 
+jsdocDir = "./jsdoc-toolkit"
+
+def getJSDocForSphinx():
+    if not os.path.exists('jsdoc-toolkit/templates/jsdoc-for-sphinx'):
+        subprocess.Popen(['git',
+                          'clone',
+                          'git://github.com/jterrace/jsdoc-for-sphinx.git',
+                          'jsdoc-toolkit/templates/jsdoc-for-sphinx']).communicate()
+
 def getJSDoc():
     if not os.path.exists('jsdoc-toolkit'):
         subprocess.Popen(['svn', 'checkout', 'http://jsdoc-toolkit.googlecode.com/svn/trunk/jsdoc-toolkit', 'jsdoc-toolkit']).communicate()
 
-def runJSDoc(emroot, outFold):
-    jsdocDir = "./jsdoc-toolkit"
-    templateDir = jsdocDir + '/templates/jsdoc'
+def runJSDoc(emroot, outFold, templateDir):
     libroot = emroot + '/scripts/std/'
-    cmd = 'bash %s/jsrun.sh -ap -t=%s -x=em -r 10 -d=%s %s'%(jsdocDir, templateDir, outFold, libroot)
+    cmd = 'bash %s/jsrun.sh -ap -t=%s -u -x=em -r=10 -d=%s %s' % (jsdocDir, templateDir, outFold, libroot)
     envir = {}
     envir["JSDOCDIR"] = jsdocDir
     subprocess.Popen(cmd.split(), env=envir).communicate()[0]
@@ -32,6 +39,7 @@ def runJSDoc(emroot, outFold):
 if __name__ == "__main__":
     outFold = None
     emroot = None
+    templateDir = jsdocDir + '/templates/jsdoc'
     for s in range(1,len(sys.argv)):
         if (sys.argv[s] == '-outFold'):
             if (s+1 < len(sys.argv)):
@@ -40,9 +48,15 @@ if __name__ == "__main__":
         if (sys.argv[s] == '-emroot'):
           if (s + 1 < len(sys.argv)):
             emroot = sys.argv[s + 1]
+            
+        if (sys.argv[s] == '--sphinx'):
+            templateDir = jsdocDir + '/templates/jsdoc-for-sphinx'
 
     if ((outFold == None) or (emroot == None)):
         print("Usage error.  Specify emerson root folder (-emroot) and output folder (-outFold)")
     else:
+        emroot = os.path.abspath(emroot)
+        outFold = os.path.abspath(outFold)
         getJSDoc()
-        runJSDoc(emroot, outFold)
+        getJSDocForSphinx()
+        runJSDoc(emroot, outFold, templateDir)
