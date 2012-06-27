436ebb49578664a9d1e79f1cf2e4504712640d2c
Jeff Terrace <jterrace@gmail.com>
2012-06-27 12:14:56 -0400

    Backport subprocess.check_output for python 2.6

--- tools/doc/fullDocs.py
+++ tools/doc/fullDocs.py
@@ -5,6 +5,31 @@ import subprocess
 import os.path
 import shutil
 
+def _check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte string.
+
+    Backported from Python 2.7 as it's implemented as pure python on stdlib.
+
+    >>> check_output(['/usr/bin/python', '--version'])
+    Python 2.6.2
+    """
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        error = subprocess.CalledProcessError(retcode, cmd)
+        error.output = output
+        raise error
+    return output
+
+try:
+    subprocess.check_output
+except AttributeError:
+    subprocess.check_output = _check_output
+
 CURDIR = os.path.dirname(os.path.abspath(__file__))
 
 EMERSON_BUILDER = os.path.join(CURDIR, 'emersonDocs.py')
